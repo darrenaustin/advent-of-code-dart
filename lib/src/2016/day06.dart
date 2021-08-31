7367052fb74a281883dbf5f959dec4adcf8b0475
Darren Austin <darren@darrenaustin.org>
2021-08-30 19:22:59 -0700

    Solved 2016 Day 6.

--- lib/src/2016/day06.dart
+++ lib/src/2016/day06.dart
@@ -1,17 +1,35 @@
 // https://adventofcode.com/2016/day/6
 
 import '../../day.dart';
+import '../util/comparison.dart';
 
 class Day06 extends AdventDay {
-  Day06() : super(2016, 6);
+  Day06() : super(2016, 6, solution1: 'kjxfwkdh', solution2: 'xrwcsnps');
 
   @override
   dynamic part1() {
-    return null;
+    return errorCorrectedMessage(inputRecordedMessages(), numMaxComparator);
   }
 
   @override
   dynamic part2() {
-    return null;
+    return errorCorrectedMessage(inputRecordedMessages(), numMinComparator);
   }
+
+  Iterable<List<String>> inputRecordedMessages() => inputDataLines().map((s) => s.split('').toList());
+
+  String errorCorrectedMessage(Iterable<List<String>> recorded, Comparator<int> freqCompare) {
+    final StringBuffer message = StringBuffer();
+    for (int i = 0; i < recorded.first.length; i++) {
+      final Map<String, int> charFrequency = <String, int>{};
+      for (var c in recorded) {
+        charFrequency[c[i]] = (charFrequency[c[i]] ?? 0) + 1;
+      }
+      final List<String> sortedChars = charFrequency.keys.toList()
+        ..sort((k1, k2) => freqCompare(charFrequency[k1]!, charFrequency[k2]!));
+      message.write(sortedChars.first);
+    }
+    return message.toString();
+  }
+
 }
