1629167f11624639caa3fc5b9e6f4aadd1e54f96
Darren Austin <darren@darrenaustin.org>
2021-12-07 22:36:26 -0800

    Solved 2021 Day 8 in a really inelegant way.

--- lib/src/2021/day08.dart
+++ lib/src/2021/day08.dart
@@ -3,15 +3,76 @@
 import '../../day.dart';
 
 class Day08 extends AdventDay {
-  Day08() : super(2021, 8);
+  Day08() : super(2021, 8, solution1: 539, solution2: 1084606);
 
   @override
   dynamic part1() {
-    return null;
+    final entries = inputEntries();
+    int easyCount = 0;
+    for (final entry in entries) {
+      final easy = {
+        entry.signals.firstWhere((s) => s.length == 2), // 1
+        entry.signals.firstWhere((s) => s.length == 4), // 4
+        entry.signals.firstWhere((s) => s.length == 3), // 7
+        entry.signals.firstWhere((s) => s.length == 7), // 8
+      };
+      easyCount += (entry.outputs.where((o) => easy.contains(o))).length;
+    }
+    return easyCount;
   }
 
   @override
   dynamic part2() {
-    return null;
+    final entries = inputEntries();
+    var sum = 0;
+    for (final entry in entries) {
+      final codes = List<String>.filled(10, '');
+      codes[1] = entry.signals.firstWhere((s) => s.length == 2);
+      codes[4] = entry.signals.firstWhere((s) => s.length == 4);
+      codes[7] = entry.signals.firstWhere((s) => s.length == 3);
+      codes[8] = entry.signals.firstWhere((s) => s.length == 7);
+
+      var d5 = Set.of(entry.signals.where((s) => s.length == 5));
+      codes[3] = d5.firstWhere((s) => s.contains(codes[1][0]) && s.contains(codes[1][1]));
+      d5.remove(codes[3]);
+
+      var left4 = codes[4].split('').where((s) => !codes[1].contains(s)).join();
+      codes[5] = d5.firstWhere((s) => s.contains(left4[0]) && s.contains(left4[1]));
+      d5.remove(codes[5]);
+      codes[2] = d5.first;
+
+      var d6 = Set.of(entry.signals.where((s) => s.length == 6));
+      codes[6] = d6.firstWhere((s) => !s.contains(codes[1][0]) || !s.contains(codes[1][1]));
+      d6.remove(codes[6]);
+
+      codes[9] = d6.firstWhere((s) => s.contains(codes[4][0]) && s.contains(codes[4][1]) && s.contains(codes[4][2]) && s.contains(codes[4][3]));
+      d6.remove(codes[9]);
+      codes[0] = d6.first;
+
+      final digits = Map.fromEntries(codes.asMap().entries.map((e) => MapEntry(e.value, e.key)));
+      sum += int.parse(entry.outputs.map((o) => digits[o]!).join());
+    }
+    return sum;
+  }
+
+  Iterable<Entry> inputEntries() {
+    return inputDataLines().map((l) {
+      final parts = l.split('|');
+      final signals = parts[0].trim().split(' ').map((w) => (w.split('')..sort()).join()).toList();
+      final outputs = parts[1].trim().split(' ').map((w) => (w.split('')..sort()).join()).toList();
+      return Entry(signals, outputs);
+    });
+  }
+}
+
+class Entry {
+  Entry(this.signals, this.outputs);
+
+  final List<String> signals;
+  final List<String> outputs;
+
+  @override
+  String toString() {
+    return '$signals, $outputs';
   }
 }
