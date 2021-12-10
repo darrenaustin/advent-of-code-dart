94c09afab94274bd4b31a39b70d7e0150c049442
Darren Austin <darren@darrenaustin.org>
2021-12-09 21:38:15 -0800

    Solved 2021 Day 10.

--- lib/src/2021/day10.dart
+++ lib/src/2021/day10.dart
@@ -3,15 +3,77 @@
 import '../../day.dart';
 
 class Day10 extends AdventDay {
-  Day10() : super(2021, 10);
+  Day10() : super(2021, 10, solution1: 318081, solution2: 4361305341);
+
+  static const pairs = {
+    '(': ')',
+    '[': ']',
+    '{': '}',
+    '<': '>',
+  };
+
+  static const corruptScore = {
+    ')': 3,
+    ']': 57,
+    '}': 1197,
+    '>': 25137,
+  };
+
+  static const incompleteScore = {
+    ')': 1,
+    ']': 2,
+    '}': 3,
+    '>': 4,
+  };
 
   @override
   dynamic part1() {
-    return null;
+    int score = 0;
+    for (final line in inputDataLines()) {
+      List stack = [];
+      for (final c in line.split('')) {
+        if (pairs.containsKey(c)) {
+          stack.add(pairs[c]);
+        } else {
+          if (stack.last == c) {
+            stack.removeLast();
+          } else {
+            score += corruptScore[c]!;
+            break;
+          }
+        }
+      }
+    }
+    return score;
   }
 
   @override
   dynamic part2() {
-    return null;
+    List<int> scores= [];
+    for (final line in inputDataLines()) {
+      List stack = [];
+      bool corrupted = false;
+      for (final c in line.split('')) {
+        if (pairs.containsKey(c)) {
+          stack.insert(0, pairs[c]);
+        } else {
+          if (stack.first == c) {
+            stack.removeAt(0);
+          } else {
+            corrupted = true;
+            break;
+          }
+        }
+      }
+      if (!corrupted) {
+        int lineScore = 0;
+        for (int i = 0; i < stack.length; i++) {
+          lineScore = lineScore * 5 + incompleteScore[stack[i]]!;
+        }
+        scores.add(lineScore);
+      }
+    }
+    scores.sort();
+    return scores[scores.length ~/ 2];
   }
 }
