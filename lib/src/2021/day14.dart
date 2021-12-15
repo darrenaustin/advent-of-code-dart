eee89dc845e6b4c827afb06e9883a1b01fd06922
Darren Austin <darren@darrenaustin.org>
2021-12-14 23:05:17 -0800

    Solved second part of 2021 Day 14 with a reasonably fast solution.

--- lib/src/2021/day14.dart
+++ lib/src/2021/day14.dart
@@ -5,7 +5,7 @@ import 'package:advent_of_code_dart/src/util/collection.dart';
 import '../../day.dart';
 
 class Day14 extends AdventDay {
-  Day14() : super(2021, 14, solution1: 2975);
+  Day14() : super(2021, 14, solution1: 2975, solution2: 3015383850689);
 
   @override
   dynamic part1() {
@@ -38,6 +38,45 @@ class Day14 extends AdventDay {
 
   @override
   dynamic part2() {
-  }
+    final template = inputDataLines().first.split('');
+    final rules = Map.fromEntries(inputDataLines().skip(1).map((l)  {
+      final parts = l.split(' -> ');
+      return MapEntry(parts[0], parts[1]);
+    }));
+    final rulePairs = Map.fromEntries(rules.entries.map((e) {
+      final chars = e.key.split('');
+      return MapEntry(e.key, [chars[0] + e.value, e.value + chars[1]].where(rules.keys.contains));
+    }));
 
+    final charCounts = <String, int>{};
+    for (final ch in template) {
+      charCounts[ch] = (charCounts[ch] ?? 0) + 1;
+    }
+    final initialPairs = range(0, template.length - 1)
+        .map((i) => template[i] + template[i + 1])
+        .where(rules.containsKey)
+        .toList();
+    var pairCounts = <String, int>{};
+    for (final pair in initialPairs) {
+      pairCounts[pair] = (pairCounts[pair] ?? 0) + 1;
+    }
+
+    for (int step = 0; step < 40; step++) {
+      final newPairCounts = <String, int>{};
+      for (final pairCount in pairCounts.entries) {
+        final pair = pairCount.key;
+        final count = pairCount.value;
+        final newChar = rules[pair]!;
+        charCounts[newChar] = (charCounts[newChar] ?? 0) + count;
+        final newPairs = rulePairs[pair]!;
+        for (final newPair in newPairs) {
+          newPairCounts[newPair] = (newPairCounts[newPair] ?? 0) + count;
+        }
+      }
+      pairCounts = newPairCounts;
+    }
+
+    final counts = charCounts.values.toList()..sort();
+    return counts.last - counts.first;
+  }
 }
