8d761a8b6fa5516b353e15ffdc4257ca86319fb6
Darren Austin <darren@darrenaustin.org>
2021-12-21 20:43:16 -0800

    Solved 2021 Day 21 part 1

--- lib/src/2021/day21.dart
+++ lib/src/2021/day21.dart
@@ -1,17 +1,50 @@
 // https://adventofcode.com/2021/day/21
 
+import '../util/collection.dart';
+
 import '../../day.dart';
 
 class Day21 extends AdventDay {
-  Day21() : super(2021, 21);
+  Day21() : super(2021, 21, solution1: 853776);
 
   @override
   dynamic part1() {
-    return null;
+    final lines = inputDataLines();
+    final p1 = lines[0].split(' ');
+    final p2 = lines[1].split(' ');
+
+    final pos = [int.parse(p1[4]), int.parse(p2[4])];
+    final score = [0, 0];
+    final die = DeterministicDie();
+    int player = 0;
+
+    bool playerWon = false;
+    while (!playerWon) {
+      pos[player] = (pos[player] + die.roll3() - 1) % 10 + 1;
+      score[player] += pos[player];
+      playerWon = score[player] >= 1000;
+      player = (player + 1) % 2;
+    }
+    return score.min() * die.numRolls;
   }
 
   @override
   dynamic part2() {
-    return null;
+  }
+}
+
+class DeterministicDie {
+  int numRolls = 0;
+  int value = 1;
+
+  int roll() {
+    final result = value;
+    value = (value % 100) + 1;
+    numRolls++;
+    return result;
+  }
+
+  int roll3() {
+    return roll() + roll() + roll();
   }
 }
