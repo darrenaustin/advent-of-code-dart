f036981eabcaccd0c463b7de1ad15a5dec6ed059
Darren Austin <darren@darrenaustin.org>
2021-12-31 17:10:56 -0800

    Cleaned up 2021 day 20.

--- lib/src/2021/day20.dart
+++ lib/src/2021/day20.dart
@@ -10,28 +10,30 @@ class Day20 extends AdventDay {
   @override
   dynamic part1() {
     final inputLines = inputDataLines();
-    final algo = inputLines.first.split('').map((s) => s == '#' ? 1 : 0).toList();
+    final algo = inputAlgorithm(inputLines.first);
+    var grid = inputGrid(inputLines);
 
-    var grid = Grid<int>(inputLines[1].length, inputLines.length - 1, 0);
-    int y = 0;
-    for (final line in inputLines.skip(1)) {
-      for (int x = 0; x < line.length; x++) {
-        grid.setCell(Loc(x, y), line[x] == '#' ? 1 : 0);
-      }
-      y++;
-    }
-
-    grid = _enhance(algo, grid, 0);
-    grid = _enhance(algo, grid, algo[0]);
-    return grid.cellsWhere((l) => l == 1).length;
+    return _enhance(algo: algo, grid: grid, numTimes: 2)
+      .cellsWhere((l) => l == 1)
+      .length;
   }
 
   @override
   dynamic part2() {
     final inputLines = inputDataLines();
-    final algo = inputLines.first.split('').map((s) => s == '#' ? 1 : 0).toList();
+    final algo = inputAlgorithm(inputLines.first);
+    final grid = inputGrid(inputLines);
 
-    var grid = Grid<int>(inputLines[1].length, inputLines.length - 1, 0);
+    return _enhance(algo: algo, grid: grid, numTimes: 50)
+      .cellsWhere((l) => l == 1)
+      .length;
+  }
+
+  List<int> inputAlgorithm(String s) =>
+    s.split('').map((s) => s == '#' ? 1 : 0).toList();
+
+  Grid<int> inputGrid(List<String> inputLines) {
+    final grid = Grid<int>(inputLines[1].length, inputLines.length - 1, 0);
     int y = 0;
     for (final line in inputLines.skip(1)) {
       for (int x = 0; x < line.length; x++) {
@@ -39,12 +41,7 @@ class Day20 extends AdventDay {
       }
       y++;
     }
-
-    grid = _enhance(algo, grid, 0);
-    for (int i = 0; i < 49; i++) {
-      grid = _enhance(algo, grid, i.isEven ? algo.first : algo.last);
-    }
-    return grid.cellsWhere((l) => l == 1).length;
+    return grid;
   }
 
   static final _areaOffsets = [
@@ -52,19 +49,29 @@ class Day20 extends AdventDay {
     Loc(-1, 0), Loc(0, 0), Loc(1, 0),
     Loc(-1, 1), Loc(0, 1), Loc(1, 1),
   ].map((o) => o + Loc(-2, -2));
-  Grid<int> _enhance(List<int> algo, Grid<int>grid, int defaultValue) {
-    final newGrid = Grid<int>(grid.width + 4, grid.height + 4, 0);
-    for (int y = 0; y < newGrid.height; y++) {
-      for (int x = 0; x < newGrid.width; x++) {
-        final center = Loc(x, y);
-        final areaValues = _areaOffsets.map((o) {
-          final l = o + center;
-          return grid.validCell(l) ? grid.cell(l) : defaultValue;
-        });
-        final index = areaValues.reduce((v, e) => (v << 1) | e);
-        newGrid.setCell(center, algo[index]);
+
+  Grid<int> _enhance({
+    required List<int> algo,
+    required Grid<int> grid,
+    int outerValue = 0,
+    int numTimes = 1,
+  }) {
+    for (int i = 0; i < numTimes; i++) {
+      final newGrid = Grid<int>(grid.width + 4, grid.height + 4, 0);
+      for (int y = 0; y < newGrid.height; y++) {
+        for (int x = 0; x < newGrid.width; x++) {
+          final center = Loc(x, y);
+          final areaValues = _areaOffsets.map((o) {
+            final l = o + center;
+            return grid.validCell(l) ? grid.cell(l) : outerValue;
+          });
+          final index = areaValues.reduce((v, e) => (v << 1) | e);
+          newGrid.setCell(center, algo[index]);
+        }
       }
+      grid = newGrid;
+      outerValue = outerValue == 0 ? algo.first : algo.last;
     }
-    return newGrid;
+    return grid;
   }
 }
