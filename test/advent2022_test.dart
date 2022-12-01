fedde3058591c8a119038ca32aebab3a61911e8a
Darren Austin <darren@darrenaustin.org>
2022-11-30 20:52:05 -0800

    Setup for 2022.

---
+++ test/advent2022_test.dart
@@ -0,0 +1,10 @@
+import 'package:advent_of_code_dart/days2022.dart';
+import 'package:test/test.dart';
+
+import 'test_days.dart';
+
+void main() {
+  group('2022', () {
+    testDays(adventOfCode2022Days);
+  });
+}
