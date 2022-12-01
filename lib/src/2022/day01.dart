c48225c71131c1a21de7adbfb2384e621f444b1d
Darren Austin <darren@darrenaustin.org>
2022-11-30 21:20:49 -0800

    Solved 2022 Day 1.

--- lib/src/2022/day01.dart
+++ lib/src/2022/day01.dart
@@ -1,17 +1,30 @@
 // https://adventofcode.com/2022/day/1
 
+import 'package:advent_of_code_dart/src/util/comparison.dart';
+import 'package:collection/collection.dart';
+
 import '../../day.dart';
 
 class Day01 extends AdventDay {
-  Day01() : super(2022, 1);
+  Day01() : super(2022, 1, solution1: 67622, solution2: 201491);
 
   @override
   dynamic part1() {
-    return null;
+    return inputCalories().max;
   }
 
   @override
   dynamic part2() {
-    return null;
+    return (inputCalories()..sort(numMaxComparator)).take(3).sum;
+  }
+
+  List<int> inputCalories() {
+    return inputData()
+      .split('\n\n')
+      .map((String group) => group
+        .split('\n')
+        .map(int.parse)
+        .sum)
+      .toList();
   }
 }
