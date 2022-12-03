47e307cc6b0ffffd634c9b5170fee72eee29bfc1
Darren Austin <darren@darrenaustin.org>
2022-12-02 22:12:12 -0800

    Solved 2022 Day 3.

--- lib/days2022.dart
+++ lib/days2022.dart
@@ -1,8 +1,10 @@
 import 'day.dart';
 import 'src/2022/day01.dart' show Day01;
 import 'src/2022/day02.dart' show Day02;
+import 'src/2022/day03.dart' show Day03;
 
 final List<AdventDay> adventOfCode2022Days = <AdventDay>[
   Day01(),
   Day02(),
+  Day03(),
 ];
---
+++ lib/src/2022/day03.dart
@@ -0,0 +1,56 @@
+// https://adventofcode.com/2022/day/3
+
+import 'package:advent_of_code_dart/src/util/collection.dart' show IterableExtensions;
+import 'package:collection/collection.dart';
+
+import '../../day.dart';
+
+class Day03 extends AdventDay {
+  Day03() : super(2022, 3, solution1: 7821, solution2: 2752);
+
+  @override
+  dynamic part1() {
+    return inputDataLines()
+      .map(compartments)
+      .map(commonItem)
+      .map(priority)
+      .sum;
+  }
+
+  @override
+  dynamic part2() {
+    return inputDataLines().partition(3)
+      .map(commonItem)
+      .map(priority)
+      .sum;
+  }
+
+  Iterable<String> compartments(String rucksack) {
+    final int middle = rucksack.length ~/ 2;
+    return [
+      rucksack.substring(0, middle),
+      rucksack.substring(middle),
+    ];
+  }
+
+  String commonItem(Iterable<String> sacks) {
+    return sacks.skip(1)
+      .fold<Set<String>>(
+        chars(sacks.first),
+        (Set<String> common, String sack) => common.intersection(chars(sack))
+      )
+      .first;
+  }
+
+  Set<String> chars(String s) => { ...s.split('') };
+
+  final int lowerA = 'a'.codeUnitAt(0);
+  final int upperA = 'A'.codeUnitAt(0);
+
+  int priority(String s) {
+    if (s.contains(RegExp(r'[a-z]'))) {
+      return s.codeUnitAt(0) - lowerA + 1;
+    }
+    return s.codeUnitAt(0) - upperA + 27;
+  }
+}
