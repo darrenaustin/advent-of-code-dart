aeb4d9e8f40a8011ba5a9938ebc85ee149707297
Darren Austin <darren@darrenaustin.org>
2022-12-11 17:56:06 -0800

    Somewhat improved the performance of 2016 day 5.
    Still very slow, but probably the best I am going to get with the md5 support in package crypto.

--- lib/src/2016/day05.dart
+++ lib/src/2016/day05.dart
@@ -9,58 +9,89 @@ import '../../day.dart';
 class Day05 extends AdventDay {
   Day05() : super(2016, 5, solution1: '2414bc77', solution2: '437e60fc');
 
-  @override
-  dynamic part1() {
-    return password(inputData());
-  }
+  // These are still very slow, but I think this is as fast as I can
+  // get it using the standard md5 support in the crypto package.
 
   @override
-  dynamic part2() {
-    // return '437e60fc';
-    return positionalPassword(inputData());
-  }
+  dynamic part1() => passwordFromHashes(inputData());
+
+  @override
+  dynamic part2() => passwordFromHashIndex(inputData());
 }
 
-String password(String id) {
-  return passwordHashes(id)
-      .take(8)
-      .map((h) => h[5])
-      .join();
-}
+final int zero = '0'.codeUnitAt(0);
+final int one = '1'.codeUnitAt(0);
+final int nine = '9'.codeUnitAt(0);
 
-String positionalPassword(String id) {
-  final chars = List<String>.filled(8, '_');
-  int charsFound = 0;
-  Iterable<String> hashes = passwordHashes(id);
-  while (charsFound < 8) {
-    final hash = hashes.first;
-    final int? index = int.tryParse(hash[5]);
-    if (index != null &&
-        0 <= index && index < 8 &&
-        chars[index] == '_') {
-      chars[index] = hash[6];
-      charsFound++;
-      // print(chars.join());
+String passwordFromHashes(String id) {
+  int password = 0;
+  int digitsFound = 0;
+  List<int> encoded = [...utf8.encode(id)];
+  int startIndex = encoded.length;
+  encoded.add(zero);
+
+  while (digitsFound < 8) {
+    final digest = md5.convert(encoded);
+    if (digest.bytes[0] == 0 && digest.bytes[1] == 0 && digest.bytes[2] < 16) {
+      password = (password << 4) | digest.bytes[2];
+      digitsFound += 1;
     }
-    hashes = hashes.skip(1);
+    // Manually increment the index in the encoded data
+    int endIndex = encoded.length - 1;
+    while (endIndex > startIndex) {
+      if (encoded[endIndex] < nine) {
+        encoded[endIndex] += 1;
+        break;
+      }
+      encoded[endIndex] = zero;
+      endIndex--;
+    }
+    if (endIndex == startIndex) {
+      if (encoded[endIndex] == nine) {
+        encoded[endIndex] = one;
+        encoded.add(zero);
+      } else {
+        encoded[endIndex] += 1;
+      }
+    }
   }
-  return chars.join();
+  return password.toRadixString(16);
 }
 
-// TODO: this is WAY too slow for this. Look into
-//       ways to speed this up.
-String textMD5(String text) {
-  return md5.convert(utf8.encode(text)).toString();
-}
+String passwordFromHashIndex(String id) {
+  int password = 0;
+  Set<int> digitsFound = {};
+  List<int> encoded = [...utf8.encode(id)];
+  int startIndex = encoded.length;
+  encoded.add(zero);
 
-Iterable<String> passwordHashes(String id) sync* {
-  int index = 0;
-  late String hash;
-  while (true) {
-    do {
-      hash = textMD5('$id${index.toString()}');
-      index++;
-    } while (!hash.startsWith('00000'));
-    yield hash;
+  while (digitsFound.length < 8) {
+    final digest = md5.convert(encoded);
+    if (digest.bytes[0] == 0 && digest.bytes[1] == 0 &&
+        digest.bytes[2] < 8 && !digitsFound.contains(digest.bytes[2])) {
+      int index = digest.bytes[2];
+      int char = digest.bytes[3] >> 4;
+      password |= char << ((7 - index) << 2);
+      digitsFound.add(index);
+    }
+    // Manually increment the index in the encoded data
+    int endIndex = encoded.length - 1;
+    while (endIndex > startIndex) {
+      if (encoded[endIndex] < nine) {
+        encoded[endIndex] += 1;
+        break;
+      }
+      encoded[endIndex] = zero;
+      endIndex--;
+    }
+    if (endIndex == startIndex) {
+      if (encoded[endIndex] == nine) {
+        encoded[endIndex] = one;
+        encoded.add(zero);
+      } else {
+        encoded[endIndex] += 1;
+      }
+    }
   }
+  return password.toRadixString(16).padLeft(8, '0');
 }
