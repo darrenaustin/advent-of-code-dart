bccb3181618eb0e14903ace333e4118dbd9f7582
Darren Austin <darren@darrenaustin.org>
2022-12-14 18:18:55 -0800

    Minor cleanup of sparse grid.

--- lib/src/util/sparse_grid.dart
+++ lib/src/util/sparse_grid.dart
@@ -25,9 +25,11 @@ class SparseGrid<T> {
   T cell(Vec2 p) => _cells[p] ?? defaultValue;
 
   void setCell(Vec2 p, T value) {
+    if (!isSet(p)) {
+      _min = Vec2(min(_min.x, p.x), min(_min.y, p.y));
+      _max = Vec2(max(_max.x, p.x), max(_max.y, p.y));
+    }
     _cells[p] = value;
-    _min = Vec2(min(_min.x, p.x), min(_min.y, p.y));
-    _max = Vec2(max(_max.x, p.x), max(_max.y, p.y));
   }
 
   int numSetCells() => _cells.length;
@@ -40,10 +42,11 @@ class SparseGrid<T> {
     return
       range(_min.yInt, _max.yInt + 1)
         .map((int y) => range(_min.xInt, _max.xInt + 1)
-        .map((int x) {
-          final Vec2 p = Vec2.int(x, y);
-          return isSet(p) ? cell(p).toString() : ' ';
-        }).join(' '))
+          .map((int x) {
+            final Vec2 p = Vec2.int(x, y);
+            return isSet(p) ? cell(p).toString() : ' ';
+          })
+          .join(' '))
         .join('\n');
   }
 }
