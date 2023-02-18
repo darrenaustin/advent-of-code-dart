81d45dfea4b029aa94a0fe9d38d10887499bf169
Darren Austin <darren@darrenaustin.org>
2023-02-18 15:41:54 -0800

    Migrated 2021 15.

--- packages/aoc2021/test/day15_test.dart
+++ packages/aoc2021/test/day15_test.dart
@@ -3,18 +3,30 @@ import 'package:test/test.dart';
 
 main() {
   group('2021 Day 15', () {
+    final exampleInput = '''
+1163751742
+1381373672
+2136511328
+3694931569
+7463417111
+1319128137
+1359912421
+3125421639
+1293138521
+2311944581''';
+
     group('part 1', () {
-      // test('examples', () {
-      //   expect(Day15().part1(''), 0); 
-      // });
+      test('example', () {
+        expect(Day15().part1(exampleInput), 40);
+      });
 
       test('solution', () => Day15().testPart1());
     });
 
     group('part 2', () {
-      // test('examples', () {
-      //   expect(Day15().part2(''), 0); 
-      // });
+      test('example', () {
+        expect(Day15().part2(exampleInput), 315);
+      });
 
       test('solution', () => Day15().testPart2());
     });
