f90740d7ab8472de0e054277b3c07d99b75ea8be
Darren Austin <darren@darrenaustin.org>
2023-02-18 16:29:11 -0800

    Migrated 2021 16.

--- packages/aoc2021/lib/src/day16.dart
+++ packages/aoc2021/lib/src/day16.dart
@@ -1,205 +1,186 @@
 // https://adventofcode.com/2021/day/16
 
+import 'dart:math';
+
 import 'package:aoc/aoc.dart';
 
 main() => Day16().solve();
 
 class Day16 extends AdventDay {
   Day16() : super(
-    2021, 16, name: '',
+    2021, 16, name: 'Packet Decoder',
+    solution1: 984, solution2: 1015320896946,
   );
 
   @override
-  dynamic part1(String input) => 'Need to migrate';
+  dynamic part1(String input) {
+    int packetVersionSum(Packet packet) {
+      int sum = packet.version;
+      for (final child in packet.children ?? []) {
+        sum += packetVersionSum(child);
+      }
+      return sum;
+    }
+
+    return packetVersionSum(Packet.parse(BitStream(input)));
+  }
 
   @override
-  dynamic part2(String input) => 'Need to migrate';
+  dynamic part2(String input) {
+    return Packet.parse(BitStream(input)).eval();
+  }
 }
 
-// https://adventofcode.com/2021/day/16
-// 
-// import 'dart:math';
-// 
-// import 'package:aoc/aoc.dart';
-// 
-// class Day16 extends AdventDay {
-//   Day16() : super(2021, 16, solution1: 984, solution2: 1015320896946);
-// 
-//   @override
-//   dynamic part1() {
-//     int packetVersionSum(Packet packet) {
-//       int sum = packet.version;
-//       for (final child in packet.children ?? []) {
-//         sum += packetVersionSum(child);
-//       }
-//       return sum;
-//     }
-// 
-//     return packetVersionSum(Packet.parse(BitStream(inputData())));
-//   }
-// 
-//   @override
-//   dynamic part2() {
-//     return Packet.parse(BitStream(inputData())).eval();
-//   }
-// }
-// 
-// class Packet {
-//   Packet({
-//     required this.version,
-//     required this.type,
-//     this.value,
-//     this.children,
-//   });
-// 
-//   factory Packet.parse(BitStream bits) {
-//     final version = bits.next(3);
-//     final type = bits.next(3);
-//     int? value;
-//     List<Packet>? children;
-//     switch (type) {
-//       case 4:
-//         // Literal number.
-//         value = _parseLiteralNumber(bits);
-//         break;
-//       default:
-//         // Operator with sub-packets.
-//         final lengthType = bits.next();
-//         switch (lengthType) {
-//           case 0:
-//             // Total length of children.
-//             final childrenLength = bits.next(15);
-//             final startingStreamLength = bits.length;
-//             children = [];
-//             while (startingStreamLength - bits.length < childrenLength) {
-//               children.add(Packet.parse(bits));
-//             }
-//             break;
-//           case 1:
-//             // Total number of children.
-//             final numChildren = bits.next(11);
-//             children = [];
-//             for (int i = 0; i < numChildren; i++){
-//               children.add(Packet.parse(bits));
-//             }
-//             break;
-//         }
-//         break;
-//     }
-// 
-//     return Packet(version: version, type: type, value: value, children: children);
-//   }
-// 
-//   final int version;
-//   final int type;
-//   final int? value;
-//   final List<Packet>? children;
-// 
-//   int eval() {
-//     switch (type) {
-//       case 0:
-//         {
-//           // Sum
-//           int sum = 0;
-//           for (final child in children ?? <Packet>[]) {
-//             sum += child.eval();
-//           }
-//           return sum;
-//         }
-// 
-//       case 1:
-//         // Product
-//         {
-//           int product = 1;
-//           for (final child in children ?? <Packet>[]) {
-//             product *= child.eval();
-//           }
-//           return product;
-//         }
-// 
-//       case 2:
-//         // Minimum
-//         {
-//           int? value;
-//           for (final child in children ?? <Packet>[]) {
-//             final childValue = child.eval();
-//             value = (value == null) ? childValue : min(value, childValue);
-//           }
-//           return value!;
-//         }
-// 
-//       case 3:
-//         // Maximum
-//         {
-//           int? value;
-//           for (final child in children ?? <Packet>[]) {
-//             final childValue = child.eval();
-//             value = (value == null) ? childValue : max(value, childValue);
-//           }
-//           return value!;
-//         }
-// 
-//       case 4:
-//         // Literal
-//         return value!;
-// 
-//       case 5:
-//         // Greater than
-//         return children![0].eval() > children![1].eval() ? 1 : 0;
-// 
-//       case 6:
-//         // Less than
-//         return children![0].eval() < children![1].eval() ? 1 : 0;
-// 
-//       case 7:
-//         // Equal
-//         return children![0].eval() == children![1].eval() ? 1 : 0;
-//     }
-//     throw Exception('Unknown packet type');
-//   }
-// 
-//   @override
-//   String toString() {
-//     return 'Packet version: $version, type: $type, value: $value, children: $children';
-//   }
-// 
-//   static int _parseLiteralNumber(BitStream bits) {
-//     int number = 0;
-//     while (bits.next() == 1) {
-//       number = number << 4 | bits.next(4);
-//     }
-//     number = number << 4 | bits.next(4);
-//     return number;
-//   }
-// }
-// 
-// class BitStream {
-// 
-//   BitStream(String hexDigits) : _bits = [] {
-//     for (final d in hexDigits.split('')) {
-//       int value = int.parse(d, radix: 16);
-//       _bits.addAll(value.toRadixString(2).padLeft(4, '0').split('').map((b) => b == '1' ? 1 : 0));
-//     }
-//   }
-// 
-//   final List<int> _bits;
-// 
-//   bool get isEmpty => _bits.isEmpty;
-//   bool get isNotEmpty => _bits.isNotEmpty;
-//   int get length => _bits.length;
-// 
-//   int next([int numBits = 1]) {
-//     int result = 0;
-//     while (numBits > 0) {
-//       result = result << 1 | _bits.removeAt(0);
-//       numBits--;
-//     }
-//     return result;
-//   }
-// 
-//   @override
-//   String toString() {
-//     return _bits.join();
-//   }
-// }
-// 
+class Packet {
+  Packet({
+    required this.version,
+    required this.type,
+    this.value,
+    this.children,
+  });
+
+  factory Packet.parse(BitStream bits) {
+    final version = bits.next(3);
+    final type = bits.next(3);
+    int? value;
+    List<Packet>? children;
+    switch (type) {
+      case 4:
+        // Literal number.
+        value = _parseLiteralNumber(bits);
+        break;
+      default:
+        // Operator with sub-packets.
+        final lengthType = bits.next();
+        switch (lengthType) {
+          case 0:
+            // Total length of children.
+            final childrenLength = bits.next(15);
+            final startingStreamLength = bits.length;
+            children = [];
+            while (startingStreamLength - bits.length < childrenLength) {
+              children.add(Packet.parse(bits));
+            }
+            break;
+          case 1:
+            // Total number of children.
+            final numChildren = bits.next(11);
+            children = [];
+            for (int i = 0; i < numChildren; i++){
+              children.add(Packet.parse(bits));
+            }
+            break;
+        }
+        break;
+    }
+
+    return Packet(version: version, type: type, value: value, children: children);
+  }
+
+  final int version;
+  final int type;
+  final int? value;
+  final List<Packet>? children;
+
+  int eval() {
+    switch (type) {
+      case 0:
+        {
+          // Sum
+          int sum = 0;
+          for (final child in children ?? <Packet>[]) {
+            sum += child.eval();
+          }
+          return sum;
+        }
+
+      case 1:
+        // Product
+        {
+          int product = 1;
+          for (final child in children ?? <Packet>[]) {
+            product *= child.eval();
+          }
+          return product;
+        }
+
+      case 2:
+        // Minimum
+        {
+          int? value;
+          for (final child in children ?? <Packet>[]) {
+            final childValue = child.eval();
+            value = (value == null) ? childValue : min(value, childValue);
+          }
+          return value!;
+        }
+
+      case 3:
+        // Maximum
+        {
+          int? value;
+          for (final child in children ?? <Packet>[]) {
+            final childValue = child.eval();
+            value = (value == null) ? childValue : max(value, childValue);
+          }
+          return value!;
+        }
+
+      case 4:
+        // Literal
+        return value!;
+
+      case 5:
+        // Greater than
+        return children![0].eval() > children![1].eval() ? 1 : 0;
+
+      case 6:
+        // Less than
+        return children![0].eval() < children![1].eval() ? 1 : 0;
+
+      case 7:
+        // Equal
+        return children![0].eval() == children![1].eval() ? 1 : 0;
+    }
+    throw Exception('Unknown packet type');
+  }
+
+  static int _parseLiteralNumber(BitStream bits) {
+    int number = 0;
+    while (bits.next() == 1) {
+      number = number << 4 | bits.next(4);
+    }
+    number = number << 4 | bits.next(4);
+    return number;
+  }
+}
+
+class BitStream {
+
+  BitStream(String hexDigits) : _bits = [] {
+    for (final d in hexDigits.split('')) {
+      int value = int.parse(d, radix: 16);
+      _bits.addAll(value
+        .toRadixString(2)
+        .padLeft(4, '0')
+        .split('')
+        .map((b) => b == '1' ? 1 : 0)
+      );
+    }
+  }
+
+  final List<int> _bits;
+
+  bool get isEmpty => _bits.isEmpty;
+  bool get isNotEmpty => _bits.isNotEmpty;
+  int get length => _bits.length;
+
+  int next([int numBits = 1]) {
+    int result = 0;
+    while (numBits > 0) {
+      result = result << 1 | _bits.removeAt(0);
+      numBits--;
+    }
+    return result;
+  }
+}
--- packages/aoc2021/test/day16_test.dart
+++ packages/aoc2021/test/day16_test.dart
@@ -3,19 +3,100 @@ import 'package:test/test.dart';
 
 main() {
   group('2021 Day 16', () {
+    group('BitStream', () {
+      test('basics', () {
+        expect(BitStream('').isEmpty, true);
+        expect(BitStream('d2fe28').isEmpty, false);
+        expect(BitStream('D2FE28').length, 24);
+      });
+
+      test('next bits', () {
+        final bits = BitStream('D2FE28');
+        expect(bits.next(3), 6);
+        expect(bits.next(3), 4);
+        expect(bits.next(5), int.parse('10111', radix: 2));
+        expect(bits.next(5), int.parse('11110', radix: 2));
+        expect(bits.next(5), int.parse('00101', radix: 2));
+      });
+    });
+
+    group('Packet', () {
+      group('parse', () {
+        test('literal value', () {
+          final packet = Packet.parse(BitStream('D2FE28'));
+          expect(packet.version, 6);
+          expect(packet.type, 4);
+          expect(packet.value, 2021);
+        });
+
+        test('operator fixed length', () {
+          final packet = Packet.parse(BitStream('38006F45291200'));
+          expect(packet.version, 1);
+          expect(packet.type, 6);
+          expect(packet.children?.length, 2);
+          expect(packet.children?[0].value, 10);
+          expect(packet.children?[1].value, 20);
+        });
+
+        test('operator fixed num children', () {
+          final packet = Packet.parse(BitStream('EE00D40C823060'));
+          expect(packet.version, 7);
+          expect(packet.type, 3);
+          expect(packet.children?.length, 3);
+          expect(packet.children?[0].value, 1);
+          expect(packet.children?[1].value, 2);
+          expect(packet.children?[2].value, 3);
+        });
+      });
+
+      group('eval', () {
+        test('sum', () {
+          expect(Packet.parse(BitStream('C200B40A82')).eval(), 3);
+        });
+
+        test('product', () {
+          expect(Packet.parse(BitStream('04005AC33890')).eval(), 54);
+        });
+
+        test('minimum', () {
+          expect(Packet.parse(BitStream('880086C3E88112')).eval(), 7);
+        });
+
+        test('maximum', () {
+          expect(Packet.parse(BitStream('CE00C43D881120')).eval(), 9);
+        });
+
+        test('greater than', () {
+          expect(Packet.parse(BitStream('F600BC2D8F')).eval(), 0);
+        });
+
+        test('less than', () {
+          expect(Packet.parse(BitStream('D8005AC2A8F0')).eval(), 1);
+        });
+
+        test('equal to', () {
+          expect(Packet.parse(BitStream('9C005AC2F8F0')).eval(), 0);
+        });
+
+        test('expression', () {
+          // 1 + 3 = 2 * 2
+          expect(Packet.parse(BitStream('9C0141080250320F1802104A08')).eval(), 1);
+        });
+      });
+    });
+
     group('part 1', () {
-      // test('examples', () {
-      //   expect(Day16().part1(''), 0); 
-      // });
+      test('examples', () {
+        expect(Day16().part1('8A004A801A8002F478'), 16);
+        expect(Day16().part1('620080001611562C8802118E34'), 12);
+        expect(Day16().part1('C0015000016115A2E0802F182340'), 23);
+        expect(Day16().part1('A0016C880162017C3686B18A3D4780'), 31);
+      });
 
       test('solution', () => Day16().testPart1());
     });
 
     group('part 2', () {
-      // test('examples', () {
-      //   expect(Day16().part2(''), 0); 
-      // });
-
       test('solution', () => Day16().testPart2());
     });
   });
