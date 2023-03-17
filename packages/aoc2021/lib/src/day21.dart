9f2e31f4d6e3ccf7c5e1c528ff31a464a70f465a
Darren Austin <darren@darrenaustin.org>
2023-03-16 19:25:01 -0700

    Migrated 2021 21.

--- packages/aoc2021/lib/src/day21.dart
+++ packages/aoc2021/lib/src/day21.dart
@@ -1,68 +1,52 @@
 // https://adventofcode.com/2021/day/21
 
 import 'package:aoc/aoc.dart';
+import 'package:aoc/util/string.dart';
+import 'package:collection/collection.dart';
 
 main() => Day21().solve();
 
 class Day21 extends AdventDay {
   Day21() : super(
-    2021, 21, name: '',
+    2021, 21, name: 'Dirac Dice',
+    solution1: 853776,
   );
 
   @override
-  dynamic part1(String input) => 'Need to migrate';
+  dynamic part1(String input) {
+    final lines = input.lines;
+    final p1 = int.parse(lines[0].split(': ')[1]);
+    final p2 = int.parse(lines[1].split(': ')[1]);
+
+    final pos = [p1, p2];
+    final score = [0, 0];
+    final die = DeterministicDie();
+    int player = 0;
+    bool playerWon = false;
+    while (!playerWon) {
+      pos[player] = (pos[player] + die.roll3() - 1) % 10 + 1;
+      score[player] += pos[player];
+      playerWon = score[player] >= 1000;
+      player = (player + 1) % 2;
+    }
+    return score.min * die.numRolls;
+  }
 
   @override
-  dynamic part2(String input) => 'Need to migrate';
+  dynamic part2(String input) {
+  }
 }
 
-// https://adventofcode.com/2021/day/21
-// 
-// import 'package:aoc/aoc.dart';
-// import 'package:collection/collection.dart';
-// 
-// class Day21 extends AdventDay {
-//   Day21() : super(2021, 21, solution1: 853776);
-// 
-//   @override
-//   dynamic part1() {
-//     final lines = inputDataLines();
-//     final p1 = lines[0].split(' ');
-//     final p2 = lines[1].split(' ');
-// 
-//     final pos = [int.parse(p1[4]), int.parse(p2[4])];
-//     final score = [0, 0];
-//     final die = DeterministicDie();
-//     int player = 0;
-// 
-//     bool playerWon = false;
-//     while (!playerWon) {
-//       pos[player] = (pos[player] + die.roll3() - 1) % 10 + 1;
-//       score[player] += pos[player];
-//       playerWon = score[player] >= 1000;
-//       player = (player + 1) % 2;
-//     }
-//     return score.min * die.numRolls;
-//   }
-// 
-//   @override
-//   dynamic part2() {
-//   }
-// }
-// 
-// class DeterministicDie {
-//   int numRolls = 0;
-//   int value = 1;
-// 
-//   int roll() {
-//     final result = value;
-//     value = (value % 100) + 1;
-//     numRolls++;
-//     return result;
-//   }
-// 
-//   int roll3() {
-//     return roll() + roll() + roll();
-//   }
-// }
-// 
+class DeterministicDie {
+  int numRolls = 0;
+  int _current = 1;
+
+  int roll() {
+    final result = _current;
+    _current = (_current % 100) + 1;
+    numRolls++;
+    return result;
+  }
+
+  int roll3() => roll() + roll() + roll();
+}
--- packages/aoc2021/test/day21_test.dart
+++ packages/aoc2021/test/day21_test.dart
@@ -3,17 +3,22 @@ import 'package:test/test.dart';
 
 main() {
   group('2021 Day 21', () {
+    final exampleInput = '''
+Player 1 starting position: 4
+Player 2 starting position: 8
+''';
+
     group('part 1', () {
-      // test('examples', () {
-      //   expect(Day21().part1(''), 0); 
-      // });
+      test('example', () {
+        expect(Day21().part1(exampleInput), 739785);
+      });
 
       test('solution', () => Day21().testPart1());
     });
 
     group('part 2', () {
-      // test('examples', () {
-      //   expect(Day21().part2(''), 0); 
+      // test('example', () {
+      //   expect(Day21().part2(exampleInput), 444356092776315);
       // });
 
       test('solution', () => Day21().testPart2());
