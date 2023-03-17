ba4f65735cfb59951ced6f7af174358eee12f536
Darren Austin <darren@darrenaustin.org>
2023-03-16 20:52:32 -0700

    Migrated 2022 03

--- packages/aoc2022/lib/src/day03.dart
+++ packages/aoc2022/lib/src/day03.dart
@@ -1,75 +1,59 @@
 // https://adventofcode.com/2022/day/3
 
 import 'package:aoc/aoc.dart';
+import 'package:aoc/util/string.dart';
+import 'package:collection/collection.dart';
 
 main() => Day03().solve();
 
 class Day03 extends AdventDay {
   Day03() : super(
-    2022, 3, name: '',
+    2022, 3, name: 'Rucksack Reorganization',
+    solution1: 7821, solution2: 2752,
   );
 
   @override
-  dynamic part1(String input) => 'Need to migrate';
+  dynamic part1(String input) =>
+    input
+      .lines
+      .map(compartments)
+      .map(commonItem)
+      .map(priority)
+      .sum;
 
   @override
-  dynamic part2(String input) => 'Need to migrate';
-}
+  dynamic part2(String input) =>
+    input
+      .lines
+      .slices(3)
+      .map(commonItem)
+      .map(priority)
+      .sum;
 
-// https://adventofcode.com/2022/day/3
-// 
-// import 'package:aoc/aoc.dart';
-// import 'package:collection/collection.dart';
-// 
-// class Day03 extends AdventDay {
-//   Day03() : super(2022, 3, solution1: 7821, solution2: 2752);
-// 
-//   @override
-//   dynamic part1() {
-//     return inputDataLines()
-//       .map(compartments)
-//       .map(commonItem)
-//       .map(priority)
-//       .sum;
-//   }
-// 
-//   @override
-//   dynamic part2() {
-//     return inputDataLines()
-//       .slices(3)
-//       .map(commonItem)
-//       .map(priority)
-//       .sum;
-//   }
-// 
-//   Iterable<String> compartments(String rucksack) {
-//     final int middle = rucksack.length ~/ 2;
-//     return [
-//       rucksack.substring(0, middle),
-//       rucksack.substring(middle),
-//     ];
-//   }
-// 
-//   String commonItem(Iterable<String> sacks) {
-//     return sacks
-//       .skip(1)
-//       .fold<Set<String>>(
-//         chars(sacks.first),
-//         (Set<String> common, String sack) => common.intersection(chars(sack))
-//       )
-//       .first;
-//   }
-// 
-//   Set<String> chars(String s) => { ...s.split('') };
-// 
-//   final int lowerA = 'a'.codeUnitAt(0);
-//   final int upperA = 'A'.codeUnitAt(0);
-// 
-//   int priority(String s) {
-//     if (s.contains(RegExp(r'[a-z]'))) {
-//       return s.codeUnitAt(0) - lowerA + 1;
-//     }
-//     return s.codeUnitAt(0) - upperA + 27;
-//   }
-// }
-// 
+  Iterable<String> compartments(String rucksack) {
+    final int middle = rucksack.length ~/ 2;
+    return [
+      rucksack.substring(0, middle),
+      rucksack.substring(middle),
+    ];
+  }
+
+  String commonItem(Iterable<String> sacks) =>
+    sacks
+      .skip(1)
+      .fold<Set<String>>(
+        chars(sacks.first),
+        (Set<String> common, String sack) => common.intersection(chars(sack))
+      )
+      .first;
+
+  Set<String> chars(String s) => { ...s.split('') };
+
+  final int lowerA = 'a'.codeUnitAt(0);
+  final int upperA = 'A'.codeUnitAt(0);
+
+  int priority(String s) =>
+    (s.contains(RegExp(r'[a-z]')))
+      ? s.codeUnitAt(0) - lowerA + 1
+      : s.codeUnitAt(0) - upperA + 27;
+}
--- packages/aoc2022/test/day03_test.dart
+++ packages/aoc2022/test/day03_test.dart
@@ -3,18 +3,26 @@ import 'package:test/test.dart';
 
 main() {
   group('2022 Day 03', () {
+    final exampleInput = '''
+vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw''';
+
     group('part 1', () {
-      // test('examples', () {
-      //   expect(Day03().part1(''), 0); 
-      // });
+      test('example', () {
+        expect(Day03().part1(exampleInput), 157);
+      });
 
       test('solution', () => Day03().testPart1());
     });
 
     group('part 2', () {
-      // test('examples', () {
-      //   expect(Day03().part2(''), 0); 
-      // });
+      test('example', () {
+        expect(Day03().part2(exampleInput), 70);
+      });
 
       test('solution', () => Day03().testPart2());
     });
