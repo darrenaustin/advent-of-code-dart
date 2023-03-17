a463dd2057b26cbc84b537c7320514a7e9219f65
Darren Austin <darren@darrenaustin.org>
2023-03-16 23:38:58 -0700

    Migrate 2022 05.

--- packages/aoc2022/lib/src/day05.dart
+++ packages/aoc2022/lib/src/day05.dart
@@ -6,92 +6,78 @@ main() => Day05().solve();
 
 class Day05 extends AdventDay {
   Day05() : super(
-    2022, 5, name: '',
+    2022, 5, name: 'Supply Stacks',
+    solution1: 'CNSZFDVLJ', solution2: 'QNDWLMGNS',
   );
 
   @override
-  dynamic part1(String input) => 'Need to migrate';
+  dynamic part1(String input) {
+    final Dock dock = Dock(input)..executeMoves();
+    return dock.stacks.map((s) => s.last).join();
+  }
 
   @override
-  dynamic part2(String input) => 'Need to migrate';
+  dynamic part2(String input) {
+    final Dock dock = Dock(input)..executeBulkMoves();
+    return dock.stacks.map((s) => s.last).join();
+  }
 }
 
-// https://adventofcode.com/2022/day/5
-// 
-// import 'package:aoc/aoc.dart';
-// 
-// class Day05 extends AdventDay {
-//   Day05() : super(2022, 5, solution1: 'CNSZFDVLJ', solution2: 'QNDWLMGNS');
-// 
-//   @override
-//   dynamic part1() {
-//     final Dock dock = Dock(input())..executeMoves();
-//     return dock.stacks.map((s) => s.last).join();
-//   }
-// 
-//   @override
-//   dynamic part2() {
-//     final Dock dock = Dock(input())..executeBulkMoves();
-//     return dock.stacks.map((s) => s.last).join();
-//   }
-// }
-// 
-// class Dock {
-//   Dock(String input) {
-//     final inputParts = input.split('\n\n');
-//     final dockInput = inputParts[0].split('\n');
-//     final instructionInput = inputParts[1].trim().split('\n');
-// 
-//     final String stackNums = dockInput.last;
-//     final numStacks = int.parse(stackNums.trim().split(RegExp(r'\s+')).last);
-//     stacks = List.generate(numStacks, (index) => <String>[]);
-//     for (final row in dockInput.reversed.skip(1)) {
-//       int stack = 0;
-//       int itemIndex = 1;
-//       while (itemIndex < row.length) {
-//         final String item = row[itemIndex];
-//         if (item != ' ') {
-//           stacks[stack].add(item);
-//         }
-//         itemIndex += 4;
-//         stack += 1;
-//       }
-//     }
-// 
-//     moves = instructionInput
-//      .map((m) => RegExp(r'move (\d+) from (\d+) to (\d+)').firstMatch(m)!)
-//      .map((m) => Move(int.parse(m.group(1)!), int.parse(m.group(2)!), int.parse(m.group(3)!)))
-//      .toList();
-//   }
-// 
-//   late final List<List<String>> stacks;
-//   late final List<Move> moves;
-// 
-//   void executeMoves() {
-//     for (final move in moves) {
-//       for (int n = 0; n < move.amount; n++) {
-//         final item = stacks[move.from - 1].removeLast();
-//         stacks[move.to - 1].add(item);
-//       }
-//     }
-//   }
-// 
-//   void executeBulkMoves() {
-//     for (final move in moves) {
-//       final items = <String>[];
-//       for (int n = 0; n < move.amount; n++) {
-//         items.add(stacks[move.from - 1].removeLast());
-//       }
-//       stacks[move.to - 1].addAll(items.reversed);
-//     }
-//   }
-// }
-// 
-// class Move {
-//   Move(this.amount, this.from, this.to);
-// 
-//   final int amount;
-//   final int from;
-//   final int to;
-// }
-// 
+class Dock {
+  Dock(String input) {
+    final inputParts = input.split('\n\n');
+    final dockInput = inputParts[0].split('\n');
+    final instructionInput = inputParts[1].trim().split('\n');
+
+    final String stackNums = dockInput.last;
+    final numStacks = int.parse(stackNums.trim().split(RegExp(r'\s+')).last);
+    stacks = List.generate(numStacks, (index) => <String>[]);
+    for (final row in dockInput.reversed.skip(1)) {
+      int stack = 0;
+      int itemIndex = 1;
+      while (itemIndex < row.length) {
+        final String item = row[itemIndex];
+        if (item != ' ') {
+          stacks[stack].add(item);
+        }
+        itemIndex += 4;
+        stack += 1;
+      }
+    }
+
+    moves = instructionInput
+     .map((m) => RegExp(r'move (\d+) from (\d+) to (\d+)').firstMatch(m)!)
+     .map((m) => Move(int.parse(m.group(1)!), int.parse(m.group(2)!), int.parse(m.group(3)!)))
+     .toList();
+  }
+
+  late final List<List<String>> stacks;
+  late final List<Move> moves;
+
+  void executeMoves() {
+    for (final move in moves) {
+      for (int n = 0; n < move.amount; n++) {
+        final item = stacks[move.from - 1].removeLast();
+        stacks[move.to - 1].add(item);
+      }
+    }
+  }
+
+  void executeBulkMoves() {
+    for (final move in moves) {
+      final items = <String>[];
+      for (int n = 0; n < move.amount; n++) {
+        items.add(stacks[move.from - 1].removeLast());
+      }
+      stacks[move.to - 1].addAll(items.reversed);
+    }
+  }
+}
+
+class Move {
+  Move(this.amount, this.from, this.to);
+
+  final int amount;
+  final int from;
+  final int to;
+}
--- packages/aoc2022/test/day05_test.dart
+++ packages/aoc2022/test/day05_test.dart
@@ -2,19 +2,30 @@ import 'package:aoc2022/src/day05.dart';
 import 'package:test/test.dart';
 
 main() {
+  final exampleInput = '''
+    [D]
+[N] [C]
+[Z] [M] [P]
+ 1   2   3
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2''';
+
   group('2022 Day 05', () {
     group('part 1', () {
-      // test('examples', () {
-      //   expect(Day05().part1(''), 0); 
-      // });
+      test('example', () {
+        expect(Day05().part1(exampleInput), 'CMZ');
+      });
 
       test('solution', () => Day05().testPart1());
     });
 
     group('part 2', () {
-      // test('examples', () {
-      //   expect(Day05().part2(''), 0); 
-      // });
+      test('example', () {
+        expect(Day05().part2(exampleInput), 'MCD');
+      });
 
       test('solution', () => Day05().testPart2());
     });
