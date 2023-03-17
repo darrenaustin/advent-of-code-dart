2771e3de45c81f9514ca4d0755ebfb79e4e6501a
Darren Austin <darren@darrenaustin.org>
2023-03-16 23:54:37 -0700

    Migrated 2022 06

--- packages/aoc2022/lib/src/day06.dart
+++ packages/aoc2022/lib/src/day06.dart
@@ -6,41 +6,27 @@ main() => Day06().solve();
 
 class Day06 extends AdventDay {
   Day06() : super(
-    2022, 6, name: '',
+    2022, 6, name: 'Tuning Trouble',
+    solution1: 1658, solution2: 2260,
   );
 
   @override
-  dynamic part1(String input) => 'Need to migrate';
+  dynamic part1(String input) {
+    return firstMarker(input, 4);
+  }
 
   @override
-  dynamic part2(String input) => 'Need to migrate';
-}
+  dynamic part2(String input) {
+    return firstMarker(input, 14);
+  }
 
-// https://adventofcode.com/2022/day/6
-// 
-// import 'package:aoc/aoc.dart';
-// 
-// class Day06 extends AdventDay {
-//   Day06() : super(2022, 6, solution1: 1658, solution2: 2260);
-// 
-//   @override
-//   dynamic part1() {
-//     return firstMarker(inputData(), 4);
-//   }
-// 
-//   @override
-//   dynamic part2() {
-//     return firstMarker(inputData(), 14);
-//   }
-// 
-//   int? firstMarker(String s, int markerLength) {
-//     for (int i = markerLength; i <  s.length; i++) {
-//       final marker = s.substring(i - markerLength, i).split('').toSet();
-//       if (marker.length == markerLength) {
-//         return i;
-//       }
-//     }
-//     return null;
-//   }
-// }
-// 
+  int? firstMarker(String s, int markerLength) {
+    for (int i = markerLength; i <  s.length; i++) {
+      final marker = s.substring(i - markerLength, i).split('').toSet();
+      if (marker.length == markerLength) {
+        return i;
+      }
+    }
+    return null;
+  }
+}
--- packages/aoc2022/test/day06_test.dart
+++ packages/aoc2022/test/day06_test.dart
@@ -4,17 +4,25 @@ import 'package:test/test.dart';
 main() {
   group('2022 Day 06', () {
     group('part 1', () {
-      // test('examples', () {
-      //   expect(Day06().part1(''), 0); 
-      // });
+      test('examples', () {
+        expect(Day06().part1('mjqjpqmgbljsphdztnvjfqwrcgsmlb'), 7);
+        expect(Day06().part1('bvwbjplbgvbhsrlpgdmjqwftvncz'), 5);
+        expect(Day06().part1('nppdvjthqldpwncqszvftbrmjlhg'), 6);
+        expect(Day06().part1('nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg'), 10);
+        expect(Day06().part1('zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw'), 11);
+      });
 
       test('solution', () => Day06().testPart1());
     });
 
     group('part 2', () {
-      // test('examples', () {
-      //   expect(Day06().part2(''), 0); 
-      // });
+      test('examples', () {
+        expect(Day06().part2('mjqjpqmgbljsphdztnvjfqwrcgsmlb'), 19);
+        expect(Day06().part2('bvwbjplbgvbhsrlpgdmjqwftvncz'), 23);
+        expect(Day06().part2('nppdvjthqldpwncqszvftbrmjlhg'), 23);
+        expect(Day06().part2('nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg'), 29);
+        expect(Day06().part2('zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw'), 26);
+      });
 
       test('solution', () => Day06().testPart2());
     });
