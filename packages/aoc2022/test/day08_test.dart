045d944807c9e1c9c49e2fd6a75d6a25215b2965
Darren Austin <darren@darrenaustin.org>
2023-03-20 15:57:05 -0700

    Migrated 2022 08.

--- packages/aoc2022/test/day08_test.dart
+++ packages/aoc2022/test/day08_test.dart
@@ -3,18 +3,25 @@ import 'package:test/test.dart';
 
 main() {
   group('2022 Day 08', () {
+    final exampleInput = '''
+30373
+25512
+65332
+33549
+35390''';
+
     group('part 1', () {
-      // test('examples', () {
-      //   expect(Day08().part1(''), 0); 
-      // });
+      test('example', () {
+        expect(Day08().part1(exampleInput), 21);
+      });
 
       test('solution', () => Day08().testPart1());
     });
 
     group('part 2', () {
-      // test('examples', () {
-      //   expect(Day08().part2(''), 0); 
-      // });
+      test('example', () {
+        expect(Day08().part2(exampleInput), 8);
+      });
 
       test('solution', () => Day08().testPart2());
     });
