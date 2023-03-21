47377777e739d22fc9117819bd7a0db789f5bfe5
Darren Austin <darren@darrenaustin.org>
2023-03-21 15:08:39 -0700

    Migrated 2022 19.

--- packages/aoc2022/test/day19_test.dart
+++ packages/aoc2022/test/day19_test.dart
@@ -3,18 +3,21 @@ import 'package:test/test.dart';
 
 main() {
   group('2022 Day 19', () {
+    final exampleInput = '''
+Blueprint 1: Each ore robot costs 4 ore. Each clay robot costs 2 ore. Each obsidian robot costs 3 ore and 14 clay. Each geode robot costs 2 ore and 7 obsidian.
+Blueprint 2: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 8 clay. Each geode robot costs 3 ore and 12 obsidian.''';
     group('part 1', () {
-      // test('examples', () {
-      //   expect(Day19().part1(''), 0); 
-      // });
+      test('example', () {
+        expect(Day19().part1(exampleInput), 33);
+      });
 
       test('solution', () => Day19().testPart1());
     });
 
     group('part 2', () {
-      // test('examples', () {
-      //   expect(Day19().part2(''), 0); 
-      // });
+      test('example', () {
+        expect(Day19().part2(exampleInput), 56 * 62);
+      });
 
       test('solution', () => Day19().testPart2());
     });
