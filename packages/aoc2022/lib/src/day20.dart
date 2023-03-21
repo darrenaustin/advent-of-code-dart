580d1a4011556f248724224e68e7430ea724ac37
Darren Austin <darren@darrenaustin.org>
2023-03-21 15:13:15 -0700

    Migrated 2022 20.

--- packages/aoc2022/lib/src/day20.dart
+++ packages/aoc2022/lib/src/day20.dart
@@ -1,80 +1,68 @@
 // https://adventofcode.com/2022/day/20
 
 import 'package:aoc/aoc.dart';
+import 'package:aoc/util/string.dart';
+import 'package:collection/collection.dart';
 
 main() => Day20().solve();
 
 class Day20 extends AdventDay {
   Day20() : super(
-    2022, 20, name: '',
+    2022, 20, name: 'Grove Positioning System',
+    solution1: 11037, solution2: 3033720253914,
   );
 
   @override
-  dynamic part1(String input) => 'Need to migrate';
+  dynamic part1(String input) => coordinateSum(mix(parseNumbers(input)));
 
   @override
-  dynamic part2(String input) => 'Need to migrate';
+  dynamic part2(String input) => coordinateSum(mix(parseNumbers(input, 811589153), 10));
+
+  List<IndexedInt> mix(List<IndexedInt> numbers, [int numTimes = 1]) {
+    final length = numbers.length;
+    final mixed = List<IndexedInt>.from(numbers);
+    for (int i = 0; i < numTimes; i++) {
+      for (final n in numbers) {
+        final pos = mixed.indexOf(n);
+        final newPos = (pos + n.value - 1) % (length - 1) + 1;
+        mixed.removeAt(pos);
+        mixed.insert(newPos, n);
+      }
+    }
+    return mixed;
+  }
+
+  int coordinateSum(List<IndexedInt> numbers) {
+    final length = numbers.length;
+    final zeroIndex = numbers.indexWhere((n) => n.value == 0);
+    return [1000, 2000, 3000]
+      .map((e) => numbers[(e + zeroIndex) % length].value)
+      .sum;
+  }
+
+  List<IndexedInt> parseNumbers(String input, [int decryptKey = 1]) {
+    return input
+      .lines
+      .map(int.parse)
+      .mapIndexed((i, value) => IndexedInt(value * decryptKey, i))
+      .toList();
+  }
 }
 
-// https://adventofcode.com/2022/day/20
-// 
-// import 'package:aoc/aoc.dart';
-// import 'package:collection/collection.dart';
-// 
-// class Day20 extends AdventDay {
-//   Day20() : super(2022, 20, solution1: 11037, solution2: 3033720253914);
-// 
-//   @override
-//   dynamic part1() => coordinateSum(mix(inputNumbers()));
-// 
-//   @override
-//   dynamic part2() => coordinateSum(mix(inputNumbers(811589153), 10));
-// 
-//   List<IndexedInt> mix(List<IndexedInt> numbers, [int numTimes = 1]) {
-//     final length = numbers.length;
-//     final mixed = List<IndexedInt>.from(numbers);
-//     for (int i = 0; i < numTimes; i++) {
-//       for (final n in numbers) {
-//         final pos = mixed.indexOf(n);
-//         final newPos = (pos + n.value - 1) % (length - 1) + 1;
-//         mixed.removeAt(pos);
-//         mixed.insert(newPos, n);
-//       }
-//     }
-//     return mixed;
-//   }
-// 
-//   int coordinateSum(List<IndexedInt> numbers) {
-//     final length = numbers.length;
-//     final zeroIndex = numbers.indexWhere((n) => n.value == 0);
-//     return [1000, 2000, 3000]
-//       .map((e) => numbers[(e + zeroIndex) % length].value)
-//       .sum;
-//   }
-// 
-//   List<IndexedInt> inputNumbers([int decryptKey = 1]) {
-//     return inputDataLines()
-//       .map(int.parse)
-//       .mapIndexed((i, value) => IndexedInt(value * decryptKey, i))
-//       .toList();
-//   }
-// }
-// 
-// class IndexedInt {
-//   IndexedInt(this.value, this.index);
-// 
-//   final int value;
-//   final int index;
-// 
-//   @override
-//   bool operator ==(Object other) {
-//     if (identical(other, this)) return true;
-//     return other is IndexedInt
-//         && other.value == value
-//         && other.index == index;
-//   }
-// 
-//   @override
-//   int get hashCode => value.hashCode ^ index.hashCode;
-// }
-// 
+class IndexedInt {
+  IndexedInt(this.value, this.index);
+
+  final int value;
+  final int index;
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(other, this)) return true;
+    return other is IndexedInt
+        && other.value == value
+        && other.index == index;
+  }
+
+  @override
+  int get hashCode => value.hashCode ^ index.hashCode;
+}
--- packages/aoc2022/test/day20_test.dart
+++ packages/aoc2022/test/day20_test.dart
@@ -3,18 +3,27 @@ import 'package:test/test.dart';
 
 main() {
   group('2022 Day 20', () {
+    final exampleInput = '''
+1
+2
+-3
+3
+-2
+0
+4''';
+
     group('part 1', () {
-      // test('examples', () {
-      //   expect(Day20().part1(''), 0); 
-      // });
+      test('example', () {
+        expect(Day20().part1(exampleInput), 3);
+      });
 
       test('solution', () => Day20().testPart1());
     });
 
     group('part 2', () {
-      // test('examples', () {
-      //   expect(Day20().part2(''), 0); 
-      // });
+      test('example', () {
+        expect(Day20().part2(exampleInput), 1623178306);
+      });
 
       test('solution', () => Day20().testPart2());
     });
