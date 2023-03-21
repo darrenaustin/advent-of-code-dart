aae29c009060ec084636eb6a9427814f70fb6395
Darren Austin <darren@darrenaustin.org>
2023-03-21 15:55:03 -0700

    Migrated 2022 25.

--- packages/aoc2022/lib/src/day25.dart
+++ packages/aoc2022/lib/src/day25.dart
@@ -1,47 +1,34 @@
 // https://adventofcode.com/2022/day/25
 
+import 'dart:math';
+
 import 'package:aoc/aoc.dart';
+import 'package:aoc/util/string.dart';
+import 'package:collection/collection.dart';
 
 main() => Day25().solve();
 
 class Day25 extends AdventDay {
   Day25() : super(
-    2022, 25, name: '',
+    2022, 25, name: 'Full of Hot Air',
+    solution1: '20===-20-020=0001-02', solution2: AdventDay.lastStarSolution,
   );
 
   @override
-  dynamic part1(String input) => 'Need to migrate';
+  dynamic part1(String input) => intSnafu(input.lines.map(snafuInt).sum);
 
   @override
-  dynamic part2(String input) => 'Need to migrate';
-}
+  dynamic part2(String input) => AdventDay.lastStarSolution;
 
-// https://adventofcode.com/2022/day/25
-// 
-// import 'dart:math';
-// 
-// import 'package:aoc/aoc.dart';
-// import 'package:collection/collection.dart';
-// 
-// class Day25 extends AdventDay {
-//   Day25() : super(2022, 25, solution1: '20===-20-020=0001-02', solution2: '🎄 Got em all! 🎉');
-// 
-//   @override
-//   dynamic part1() => intSnafu(inputDataLines().map(snafuInt).sum);
-// 
-//   @override
-//   dynamic part2() => '🎄 Got em all! 🎉';
-// 
-//   final digitValues = { '=': -2, '-': -1, '0': 0, '1': 1, '2': 2 };
-//   int snafuInt(String s) => s.split('')
-//     .reversed
-//     .mapIndexed((p, d) => pow(5, p).toInt() * digitValues[d]!)
-//     .sum;
-// 
-//   final unit = ['=', '-', '0', '1', '2'];
-//   String intSnafu(int n) =>
-//     n == 0
-//       ? ''
-//       : intSnafu((n + 2) ~/ 5) + unit[(n + 2) % 5];
-// }
-// 
+  static final digitValues = { '=': -2, '-': -1, '0': 0, '1': 1, '2': 2 };
+  static int snafuInt(String s) => s.split('')
+    .reversed
+    .mapIndexed((p, d) => pow(5, p).toInt() * digitValues[d]!)
+    .sum;
+
+  static final unit = ['=', '-', '0', '1', '2'];
+  static String intSnafu(int n) =>
+    n == 0
+      ? ''
+      : intSnafu((n + 2) ~/ 5) + unit[(n + 2) % 5];
+}
--- packages/aoc2022/test/day25_test.dart
+++ packages/aoc2022/test/day25_test.dart
@@ -3,19 +3,66 @@ import 'package:test/test.dart';
 
 main() {
   group('2022 Day 25', () {
+    final exampleInput = '''
+1=-0-2
+12111
+2=0=
+21
+2=01
+111
+20012
+112
+1=-1=
+1-12
+12
+1=
+122''';
+
+    group('SNAFU', () {
+      test('decimal to SNAFU', () {
+        expect(Day25.intSnafu(1), '1');
+        expect(Day25.intSnafu(2), '2');
+        expect(Day25.intSnafu(3), '1=');
+        expect(Day25.intSnafu(4), '1-');
+        expect(Day25.intSnafu(5), '10');
+        expect(Day25.intSnafu(6), '11');
+        expect(Day25.intSnafu(7), '12');
+        expect(Day25.intSnafu(8), '2=');
+        expect(Day25.intSnafu(9), '2-');
+        expect(Day25.intSnafu(10), '20');
+        expect(Day25.intSnafu(15), '1=0');
+        expect(Day25.intSnafu(20), '1-0');
+        expect(Day25.intSnafu(2022), '1=11-2');
+        expect(Day25.intSnafu(12345), '1-0---0');
+        expect(Day25.intSnafu(314159265), '1121-1110-1=0');
+      });
+
+      test('SNAFU to decimal', () {
+        expect(Day25.snafuInt('1=-0-2'), 1747);
+        expect(Day25.snafuInt('12111'), 906);
+        expect(Day25.snafuInt('2=0='), 198);
+        expect(Day25.snafuInt('21'), 11);
+        expect(Day25.snafuInt('2=01'), 201);
+        expect(Day25.snafuInt('111'), 31);
+        expect(Day25.snafuInt('20012'), 1257);
+        expect(Day25.snafuInt('112'), 32);
+        expect(Day25.snafuInt('1=-1='), 353);
+        expect(Day25.snafuInt('1-12'), 107);
+        expect(Day25.snafuInt('12'), 7);
+        expect(Day25.snafuInt('1='), 3);
+        expect(Day25.snafuInt('122'), 37);
+      });
+    });
+
     group('part 1', () {
-      // test('examples', () {
-      //   expect(Day25().part1(''), 0); 
-      // });
+      test('example', () {
+        expect(Day25().part1(exampleInput), '2=-1=0');
+      });
 
       test('solution', () => Day25().testPart1());
     });
 
     group('part 2', () {
-      // test('examples', () {
-      //   expect(Day25().part2(''), 0); 
-      // });
-
       test('solution', () => Day25().testPart2());
     });
   });
