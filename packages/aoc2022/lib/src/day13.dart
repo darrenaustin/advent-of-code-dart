a672f836b570641d58693bab1a9ef95b2aaf1aab
Darren Austin <darren@darrenaustin.org>
2023-03-20 21:18:01 -0700

    Migrated 2022 13.

--- packages/aoc2022/lib/src/day13.dart
+++ packages/aoc2022/lib/src/day13.dart
@@ -1,86 +1,77 @@
 // https://adventofcode.com/2022/day/13
 
+import 'dart:convert';
+
 import 'package:aoc/aoc.dart';
+import 'package:aoc/util/string.dart';
+import 'package:collection/collection.dart';
 
 main() => Day13().solve();
 
 class Day13 extends AdventDay {
   Day13() : super(
-    2022, 13, name: '',
+    2022, 13, name: 'Distress Signal',
+    solution1: 6568, solution2: 19493,
   );
 
   @override
-  dynamic part1(String input) => 'Need to migrate';
+  dynamic part1(String input) {
+    return input
+      .lines
+      .where((l) => l.isNotEmpty)
+      .slices(2)
+      .map((pair) => pair.map(parsePacket))
+      .mapIndexed((index, pair) =>
+        comparePackets(pair.first, pair.last) == -1
+          ? index + 1
+          : null)
+      .whereNotNull()
+      .sum;
+  }
 
   @override
-  dynamic part2(String input) => 'Need to migrate';
-}
+  dynamic part2(String input) {
+    final div1 = [[2]];
+    final div2 = [[6]];
+    final packets = input
+      .lines
+      .where((l) => l.isNotEmpty)
+      .map(parsePacket)
+      .toList()
+      ..add(div1)
+      ..add(div2)
+      ..sort(comparePackets);
+    return (packets.indexOf(div1) + 1) * (packets.indexOf(div2) + 1);
+  }
 
-// https://adventofcode.com/2022/day/13
-// 
-// import 'dart:convert';
-// 
-// import 'package:aoc/aoc.dart';
-// import 'package:collection/collection.dart';
-// 
-// class Day13 extends AdventDay {
-//   Day13() : super(2022, 13, solution1: 6568, solution2: 19493);
-// 
-//   @override
-//   dynamic part1() {
-//     return inputDataLines()
-//       .slices(2)
-//       .map((pair) => pair.map(parsePacket))
-//       .mapIndexed((index, pair) =>
-//         comparePackets(pair.first, pair.last) == -1
-//           ? index + 1
-//           : null)
-//       .whereNotNull()
-//       .sum;
-//   }
-// 
-//   @override
-//   dynamic part2() {
-//     final div1 = [[2]];
-//     final div2 = [[6]];
-//     final packets = inputDataLines()
-//       .map(parsePacket)
-//       .toList()
-//       ..add(div1)
-//       ..add(div2)
-//       ..sort(comparePackets);
-//     return (packets.indexOf(div1) + 1) * (packets.indexOf(div2) + 1);
-//   }
-// 
-//   dynamic parsePacket(String s) => jsonDecode(s);
-// 
-//   int comparePackets(dynamic p1, dynamic p2) {
-//     if (p1 is num && p2 is List) {
-//       return comparePackets([p1], p2);
-//     }
-//     if (p1 is List && p2 is num) {
-//       return comparePackets(p1, [p2]);
-//     }
-//     if (p1 is num && p2 is num) {
-//       return p1.compareTo(p2);
-//     }
-// 
-//     // Compare two lists
-//     final l1 = p1 as List;
-//     final l2 = p2 as List;
-//     for (int i = 0; i < l1.length; i++) {
-//       if (i >= l2.length) {
-//         return 1;
-//       }
-//       final childCompare = comparePackets(l1[i], l2[i]);
-//       if (childCompare != 0) {
-//         return childCompare;
-//       }
-//     }
-//     if (l2.length > l1.length) {
-//       return -1;
-//     }
-//     return 0;
-//   }
-// }
-// 
+  dynamic parsePacket(String s) => jsonDecode(s);
+
+  int comparePackets(dynamic p1, dynamic p2) {
+    if (p1 is num && p2 is List) {
+      return comparePackets([p1], p2);
+    }
+    if (p1 is List && p2 is num) {
+      return comparePackets(p1, [p2]);
+    }
+    if (p1 is num && p2 is num) {
+      return p1.compareTo(p2);
+    }
+
+    // Compare two lists
+    final l1 = p1 as List;
+    final l2 = p2 as List;
+    for (int i = 0; i < l1.length; i++) {
+      if (i >= l2.length) {
+        return 1;
+      }
+      final childCompare = comparePackets(l1[i], l2[i]);
+      if (childCompare != 0) {
+        return childCompare;
+      }
+    }
+    if (l2.length > l1.length) {
+      return -1;
+    }
+    return 0;
+  }
+}
--- packages/aoc2022/test/day13_test.dart
+++ packages/aoc2022/test/day13_test.dart
@@ -3,18 +3,43 @@ import 'package:test/test.dart';
 
 main() {
   group('2022 Day 13', () {
+    final exampleInput = '''
+[1,1,3,1,1]
+[1,1,5,1,1]
+
+[[1],[2,3,4]]
+[[1],4]
+
+[9]
+[[8,7,6]]
+
+[[4,4],4,4]
+[[4,4],4,4,4]
+
+[7,7,7,7]
+[7,7,7]
+
+[]
+[3]
+
+[[[]]]
+[[]]
+
+[1,[2,[3,[4,[5,6,7]]]],8,9]
+[1,[2,[3,[4,[5,6,0]]]],8,9]''';
+
     group('part 1', () {
-      // test('examples', () {
-      //   expect(Day13().part1(''), 0); 
-      // });
+      test('example', () {
+        expect(Day13().part1(exampleInput), 13);
+      });
 
       test('solution', () => Day13().testPart1());
     });
 
     group('part 2', () {
-      // test('examples', () {
-      //   expect(Day13().part2(''), 0); 
-      // });
+      test('example', () {
+        expect(Day13().part2(exampleInput), 140);
+      });
 
       test('solution', () => Day13().testPart2());
     });
