ec34b27b636735e2443672b7da74962ccbb4c546
Darren Austin <darren@darrenaustin.org>
2023-03-21 14:58:13 -0700

    Migrated 2022 18.

--- packages/aoc2022/test/day18_test.dart
+++ packages/aoc2022/test/day18_test.dart
@@ -3,18 +3,33 @@ import 'package:test/test.dart';
 
 main() {
   group('2022 Day 18', () {
+    final exampleInput = '''
+2,2,2
+1,2,2
+3,2,2
+2,1,2
+2,3,2
+2,2,1
+2,2,3
+2,2,4
+2,2,6
+1,2,5
+3,2,5
+2,1,5
+2,3,5''';
+
     group('part 1', () {
-      // test('examples', () {
-      //   expect(Day18().part1(''), 0); 
-      // });
+      test('example', () {
+        expect(Day18().part1(exampleInput), 64);
+      });
 
       test('solution', () => Day18().testPart1());
     });
 
     group('part 2', () {
-      // test('examples', () {
-      //   expect(Day18().part2(''), 0); 
-      // });
+      test('example', () {
+        expect(Day18().part2(exampleInput), 58);
+      });
 
       test('solution', () => Day18().testPart2());
     });
