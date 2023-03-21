c92163213f5dd79140c527f88fe4e091aafd847b
Darren Austin <darren@darrenaustin.org>
2023-03-21 15:28:19 -0700

    Migrated 2022 23.

--- packages/aoc2022/lib/src/day23.dart
+++ packages/aoc2022/lib/src/day23.dart
@@ -1,132 +1,120 @@
 // https://adventofcode.com/2022/day/23
 
+import 'dart:math';
+
 import 'package:aoc/aoc.dart';
+import 'package:aoc/util/collection.dart';
+import 'package:aoc/util/string.dart';
+import 'package:aoc/util/vec2.dart';
+import 'package:collection/collection.dart';
 
 main() => Day23().solve();
 
 class Day23 extends AdventDay {
   Day23() : super(
-    2022, 23, name: '',
+    2022, 23, name: 'Unstable Diffusion',
+    solution1: 3877, solution2: 982,
   );
 
   @override
-  dynamic part1(String input) => 'Need to migrate';
+  dynamic part1(String input) {
+    final field = Field(input.lines);
+    for (final _ in range(10)) {
+      field.nextRound();
+    }
+    return field.minimalGridEmptyCells();
+  }
 
   @override
-  dynamic part2(String input) => 'Need to migrate';
+  dynamic part2(String input) {
+    final field = Field(input.lines);
+    while (field.nextRound()) {}
+    return field.round;
+  }
 }
 
-// https://adventofcode.com/2022/day/23
-// 
-// import 'dart:math';
-// 
-// import 'package:aoc/aoc.dart';
-// import 'package:aoc/util/collection.dart';
-// import 'package:aoc/util/vec2.dart';
-// import 'package:collection/collection.dart';
-// 
-// class Day23 extends AdventDay {
-//   Day23() : super(2022, 23, solution1: 3877, solution2: 982);
-// 
-//   @override
-//   dynamic part1() {
-//     final field = Field(inputDataLines());
-//     for (final _ in range(10)) {
-//       field.nextRound();
-//     }
-//     return field.minimalGridEmptyCells();
-//   }
-// 
-//   @override
-//   dynamic part2() {
-//     final field = Field(inputDataLines());
-//     while (field.nextRound()) {}
-//     return field.round;
-//   }
-// }
-// 
-// class Field {
-// 
-//   Field(List<String> data) {
-//     final gridData = data.map((l) => l.split('')).toList();
-//     for (int y = 0; y < gridData.length; y++) {
-//       for (int x = 0; x < gridData.first.length; x++) {
-//         final cell = gridData[y][x];
-//         if (cell == '#') {
-//           _elves.add(Vec2.int(x, y));
-//         }
-//       }
-//     }
-//   }
-// 
-//   int round = 0;
-//   final Set<Vec2> _elves = {};
-// 
-//   bool nextRound() {
-//     // Find all elves with at least another around them.
-//     final movingElves = _elves
-//       .where((e) => Vec2.aroundDirs.any((d) => _elves.contains(d + e)));
-// 
-//     // Determine the plans for all moving elves.
-//     final plans = <Vec2, Vec2>{};
-//     final destinations = <Vec2>{};
-//     final overbooked = <Vec2>{};
-//     for (final elf in movingElves) {
-//       for (final dir in _testDirs) {
-//         if (dir.checkDirections.map((d) => d + elf).none((p) => _elves.contains(p))) {
-//           final destination = elf + dir.direction;
-//           if (destinations.contains(destination)) {
-//             overbooked.add(destination);
-//           } else {
-//             plans[elf] = destination;
-//             destinations.add(destination);
-//           }
-//           break;
-//         }
-//       }
-//     }
-// 
-//     // Move the elves that aren't going to overbooked locations.
-//     for (final plan in plans.entries) {
-//       if (!overbooked.contains(plan.value)) {
-//         _elves.remove(plan.key);
-//         _elves.add(plan.value);
-//       }
-//     }
-// 
-//     // Rotate the test directions
-//     _testDirs.add(_testDirs.removeAt(0));
-//     round += 1;
-// 
-//     return plans.isNotEmpty;
-//   }
-// 
-//   int minimalGridEmptyCells() {
-//     // Determine minimal grid around elves.
-//     Vec2 minS = Vec2.zero;
-//     Vec2 maxS = Vec2.zero;
-//     for (final elf in _elves) {
-//       minS = Vec2(min(minS.x, elf.x), min(minS.y, elf.y));
-//       maxS = Vec2(max(maxS.x, elf.x), max(maxS.y, elf.y));
-//     }
-// 
-//     // Calculate the empty cells in the grid.
-//     final span = maxS - minS;
-//     final numCells = (span.xInt + 1) * (span.yInt + 1);
-//     return numCells - _elves.length;
-//   }
-// 
-//   // Direction to travel if there are no neighbors in the directions list.
-//   final List<CheckDirs> _testDirs = [
-//     CheckDirs(Vec2.up, [Vec2.upLeft, Vec2.up, Vec2.upRight]),
-//     CheckDirs(Vec2.down, [Vec2.downLeft, Vec2.down, Vec2.downRight]),
-//     CheckDirs(Vec2.left, [Vec2.upLeft, Vec2.left, Vec2.downLeft]),
-//     CheckDirs(Vec2.right, [Vec2.upRight, Vec2.right, Vec2.downRight]),
-//   ];
-// }
-// 
-// class CheckDirs {
-//   CheckDirs(this.direction, this.checkDirections);
-//   final Vec2 direction;
-//   final List<Vec2> checkDirections;
-// }
+class Field {
+
+  Field(List<String> data) {
+    final gridData = data.map((l) => l.split('')).toList();
+    for (int y = 0; y < gridData.length; y++) {
+      for (int x = 0; x < gridData.first.length; x++) {
+        final cell = gridData[y][x];
+        if (cell == '#') {
+          _elves.add(Vec2.int(x, y));
+        }
+      }
+    }
+  }
+
+  int round = 0;
+  final Set<Vec2> _elves = {};
+
+  bool nextRound() {
+    // Find all elves with at least another around them.
+    final movingElves = _elves
+      .where((e) => Vec2.aroundDirs.any((d) => _elves.contains(d + e)));
+
+    // Determine the plans for all moving elves.
+    final plans = <Vec2, Vec2>{};
+    final destinations = <Vec2>{};
+    final overbooked = <Vec2>{};
+    for (final elf in movingElves) {
+      for (final dir in _testDirs) {
+        if (dir.checkDirections.map((d) => d + elf).none((p) => _elves.contains(p))) {
+          final destination = elf + dir.direction;
+          if (destinations.contains(destination)) {
+            overbooked.add(destination);
+          } else {
+            plans[elf] = destination;
+            destinations.add(destination);
+          }
+          break;
+        }
+      }
+    }
+
+    // Move the elves that aren't going to overbooked locations.
+    for (final plan in plans.entries) {
+      if (!overbooked.contains(plan.value)) {
+        _elves.remove(plan.key);
+        _elves.add(plan.value);
+      }
+    }
+
+    // Rotate the test directions
+    _testDirs.add(_testDirs.removeAt(0));
+    round += 1;
+
+    return plans.isNotEmpty;
+  }
+
+  int minimalGridEmptyCells() {
+    // Determine minimal grid around elves.
+    Vec2 minS = Vec2.zero;
+    Vec2 maxS = Vec2.zero;
+    for (final elf in _elves) {
+      minS = Vec2(min(minS.x, elf.x), min(minS.y, elf.y));
+      maxS = Vec2(max(maxS.x, elf.x), max(maxS.y, elf.y));
+    }
+
+    // Calculate the empty cells in the grid.
+    final span = maxS - minS;
+    final numCells = (span.xInt + 1) * (span.yInt + 1);
+    return numCells - _elves.length;
+  }
+
+  // Direction to travel if there are no neighbors in the directions list.
+  final List<CheckDirs> _testDirs = [
+    CheckDirs(Vec2.up, [Vec2.upLeft, Vec2.up, Vec2.upRight]),
+    CheckDirs(Vec2.down, [Vec2.downLeft, Vec2.down, Vec2.downRight]),
+    CheckDirs(Vec2.left, [Vec2.upLeft, Vec2.left, Vec2.downLeft]),
+    CheckDirs(Vec2.right, [Vec2.upRight, Vec2.right, Vec2.downRight]),
+  ];
+}
+
+class CheckDirs {
+  CheckDirs(this.direction, this.checkDirections);
+  final Vec2 direction;
+  final List<Vec2> checkDirections;
+}
--- packages/aoc2022/test/day23_test.dart
+++ packages/aoc2022/test/day23_test.dart
@@ -3,18 +3,27 @@ import 'package:test/test.dart';
 
 main() {
   group('2022 Day 23', () {
+    final exampleInput = '''
+....#..
+..###.#
+#...#.#
+.#...##
+#.###..
+##.#.##
+.#..#..''';
+
     group('part 1', () {
-      // test('examples', () {
-      //   expect(Day23().part1(''), 0); 
-      // });
+      test('example', () {
+        expect(Day23().part1(exampleInput), 110);
+      });
 
       test('solution', () => Day23().testPart1());
     });
 
     group('part 2', () {
-      // test('examples', () {
-      //   expect(Day23().part2(''), 0); 
-      // });
+      test('example', () {
+        expect(Day23().part2(exampleInput), 20);
+      });
 
       test('solution', () => Day23().testPart2());
     });
