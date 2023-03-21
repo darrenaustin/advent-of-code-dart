f12843eb178ea095d3f9ba5e5632dd42ebda2ea2
Darren Austin <darren@darrenaustin.org>
2023-03-20 17:07:22 -0700

    Migrated 2022 10

--- packages/aoc2022/lib/src/day10.dart
+++ packages/aoc2022/lib/src/day10.dart
@@ -1,104 +1,94 @@
 // https://adventofcode.com/2022/day/10
 
 import 'package:aoc/aoc.dart';
+import 'package:aoc/util/string.dart';
 
 main() => Day10().solve();
 
 class Day10 extends AdventDay {
   Day10() : super(
-    2022, 10, name: '',
+    2022, 10, name: 'Cathode-Ray Tube',
+    solution1: 14620, solution2: 'BJFRHRFU',
   );
 
   @override
-  dynamic part1(String input) => 'Need to migrate';
+  dynamic part1(String input) {
+    int signalSum = 0;
+    final sampleCycles = {20, 60, 100, 140, 180, 220};
+
+    void onTick(int cycle, int x) {
+      if (sampleCycles.contains(cycle)) {
+        signalSum += cycle * x;
+      }
+    }
+
+    CPU(onTick).executeInstructions(input.lines);
+    return signalSum;
+  }
 
   @override
-  dynamic part2(String input) => 'Need to migrate';
+  dynamic part2(String input) {
+    final crt = CRT();
+    CPU((int _, int x) => crt.tick(x)).executeInstructions(input.lines);
+
+    // Don't have an OCR for this, so report from manual inspection of output.
+    print(crt);
+    return 'BJFRHRFU';
+  }
 }
 
-// https://adventofcode.com/2022/day/10
-// 
-// import 'package:aoc/aoc.dart';
-// 
-// class Day10 extends AdventDay {
-//   Day10() : super(2022, 10, solution1: 14620, solution2: 'BJFRHRFU');
-// 
-//   @override
-//   dynamic part1() {
-//     int signalSum = 0;
-//     final sampleCycles = {20, 60, 100, 140, 180, 220};
-// 
-//     void onTick(int cycle, int x) {
-//       if (sampleCycles.contains(cycle)) {
-//         signalSum += cycle * x;
-//       }
-//     }
-// 
-//     CPU(onTick).executeInstructions(inputDataLines());
-//     return signalSum;
-//   }
-// 
-//   @override
-//   dynamic part2() {
-//     final crt = CRT();
-//     CPU((int _, int x) => crt.tick(x)).executeInstructions(inputDataLines());
-// 
-//     // Don't have an OCR for this, so report from manual inspection of output.
-//     print(crt);
-//     return 'BJFRHRFU';
-//   }
-// }
-// 
-// class CPU {
-//   CPU(this.onTick);
-// 
-//   final void Function(int cycle, int x) onTick;
-// 
-//   int cycle = 0;
-//   int x = 1;
-// 
-//   void nextCycle() {
-//     cycle += 1;
-//     onTick(cycle, x);
-//   }
-// 
-//   void executeInstructions(List<String> instructions) {
-//     for (final op in instructions) {
-//       if (op == "noop") {
-//         nextCycle();
-//       } else {
-//         int value = int.parse(op.split(' ')[1]);
-//         nextCycle();
-//         nextCycle();
-//         x += value;
-//       }
-//     }
-//   }
-// }
-// 
-// class CRT {
-// 
-//   CRT([this.width = 40]);
-// 
-//   final int width;
-// 
-//   final StringBuffer _lines = StringBuffer();
-//   int _pixel = 0;
-// 
-//   void tick(int spriteX) {
-//     final paintPixel = (_pixel - spriteX).abs() < 2;
-//     final pixelChar = paintPixel ? '\u2588' : ' ';
-//     _lines.write(pixelChar);
-//     _pixel += 1;
-//     if (_pixel >= width) {
-//       _lines.writeln();
-//       _pixel = 0;
-//     }
-//   }
-// 
-//   @override
-//   String toString() {
-//     return _lines.toString();
-//   }
-// }
-// 
+class CPU {
+  CPU(this.onTick);
+
+  final void Function(int cycle, int x) onTick;
+
+  int cycle = 0;
+  int x = 1;
+
+  void nextCycle() {
+    cycle += 1;
+    onTick(cycle, x);
+  }
+
+  void executeInstructions(List<String> instructions) {
+    for (final op in instructions) {
+      if (op == "noop") {
+        nextCycle();
+      } else {
+        int value = int.parse(op.split(' ')[1]);
+        nextCycle();
+        nextCycle();
+        x += value;
+      }
+    }
+  }
+}
+
+class CRT {
+
+  CRT({
+    this.width = 40,
+    this.onPixel = '#',
+    this.offPixel = ' ',
+  });
+
+  final int width;
+  final String onPixel;
+  final String offPixel;
+
+  final StringBuffer _lines = StringBuffer();
+  int _pixel = 0;
+
+  void tick(int spriteX) {
+    if (_pixel >= width) {
+      _lines.writeln();
+      _pixel = 0;
+    }
+    final paintPixel = (_pixel - spriteX).abs() < 2;
+    _lines.write(paintPixel ? onPixel : offPixel);
+    _pixel += 1;
+  }
+
+  @override
+  String toString() => _lines.toString();
+}
--- packages/aoc2022/test/day10_test.dart
+++ packages/aoc2022/test/day10_test.dart
@@ -1,20 +1,178 @@
+import 'package:aoc/util/string.dart';
 import 'package:aoc2022/src/day10.dart';
 import 'package:test/test.dart';
 
 main() {
   group('2022 Day 10', () {
+    final exampleInput = '''
+addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop''';
+
     group('part 1', () {
-      // test('examples', () {
-      //   expect(Day10().part1(''), 0); 
-      // });
+      test('example', () {
+        expect(Day10().part1(exampleInput), 13140);
+      });
 
       test('solution', () => Day10().testPart1());
     });
 
     group('part 2', () {
-      // test('examples', () {
-      //   expect(Day10().part2(''), 0); 
-      // });
+      test('example', () {
+        final exampleOutput = '''
+##..##..##..##..##..##..##..##..##..##..
+###...###...###...###...###...###...###.
+####....####....####....####....####....
+#####.....#####.....#####.....#####.....
+######......######......######......####
+#######.......#######.......#######.....''';
+        final crt = CRT(offPixel: '.');
+        CPU((int _, int x) => crt.tick(x)).executeInstructions(exampleInput.lines);
+        expect(crt.toString(), exampleOutput);
+      });
 
       test('solution', () => Day10().testPart2());
     });
