d39ac8467c8827273a33e9c69313b4abcf63ae13
Darren Austin <darren@darrenaustin.org>
2023-03-20 21:04:47 -0700

    Migrated 2022 11.

--- packages/aoc2022/test/day11_test.dart
+++ packages/aoc2022/test/day11_test.dart
@@ -3,18 +3,47 @@ import 'package:test/test.dart';
 
 main() {
   group('2022 Day 11', () {
+    final exampleInput = '''
+Monkey 0:
+  Starting items: 79, 98
+  Operation: new = old * 19
+  Test: divisible by 23
+    If true: throw to monkey 2
+    If false: throw to monkey 3
+
+Monkey 1:
+  Starting items: 54, 65, 75, 74
+  Operation: new = old + 6
+  Test: divisible by 19
+    If true: throw to monkey 2
+    If false: throw to monkey 0
+
+Monkey 2:
+  Starting items: 79, 60, 97
+  Operation: new = old * old
+  Test: divisible by 13
+    If true: throw to monkey 1
+    If false: throw to monkey 3
+
+Monkey 3:
+  Starting items: 74
+  Operation: new = old + 3
+  Test: divisible by 17
+    If true: throw to monkey 0
+    If false: throw to monkey 1''';
+
     group('part 1', () {
-      // test('examples', () {
-      //   expect(Day11().part1(''), 0); 
-      // });
+      test('examples', () {
+        expect(Day11().part1(exampleInput), 10605);
+      });
 
       test('solution', () => Day11().testPart1());
     });
 
     group('part 2', () {
-      // test('examples', () {
-      //   expect(Day11().part2(''), 0); 
-      // });
+      test('examples', () {
+        expect(Day11().part2(exampleInput), 2713310158);
+      });
 
       test('solution', () => Day11().testPart2());
     });
