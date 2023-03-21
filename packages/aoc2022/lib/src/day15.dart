727660d45830c13dd34382e82c3458c5993eee72
Darren Austin <darren@darrenaustin.org>
2023-03-20 21:36:35 -0700

    Migrated 2022 15.

--- packages/aoc2022/lib/src/day15.dart
+++ packages/aoc2022/lib/src/day15.dart
@@ -1,63 +1,138 @@
 // https://adventofcode.com/2022/day/15
 
+import 'dart:math';
+
 import 'package:aoc/aoc.dart';
+import 'package:aoc/util/collection.dart';
+import 'package:aoc/util/string.dart';
+import 'package:aoc/util/vec2.dart';
 
 main() => Day15().solve();
 
 class Day15 extends AdventDay {
   Day15() : super(
-    2022, 15, name: '',
+    2022, 15, name: 'Beacon Exclusion Zone',
+    solution1: 5125700, solution2: 11379394658764,
   );
 
   @override
-  dynamic part1(String input) => 'Need to migrate';
+  dynamic part1(String input, [int row = 2000000]) {
+    final sensors = parseSensors(input);
+
+    int minX = 0;
+    int maxX = 0;
+    for (final s in sensors) {
+      minX = min(minX, s.pos.xInt - s.distance);
+      maxX = max(maxX, s.pos.xInt + s.distance);
+    }
+
+    bool noBeacon(Vec2 p) =>
+      sensors.any((s) =>
+        p != s.beacon &&
+        s.pos.manhattanDistanceTo(p) <= s.distance
+      );
+
+    return range(minX, maxX + 1)
+      .map((col) => Vec2.int(col, row))
+      .where(noBeacon)
+      .length;
+  }
 
   @override
-  dynamic part2(String input) => 'Need to migrate';
+  dynamic part2(String input, [int maxGrid = 4000000]) {
+    final sensors = parseSensors(input);
+    final minGrid = 0;
+    final diamondDirs = [Vec2(-1, -1), Vec2(-1, 1), Vec2(1, -1), Vec2(1, 1)];
+
+    // Given that there is only one beacon that isn't seen by all sensors
+    // in the grid, it must be at most sensor.distance+1 from all sensors.
+    // If there was a beacon at more then that, then there would be more than
+    // one option. So we just need to check the diamond+1 perimeter of each
+    // sensor.
+    for (final sensor in sensors) {
+      final distance = sensor.distance;
+      for (final dir in diamondDirs)  {
+        for (int dx = 0; dx <= distance; dx++) {
+          final dy = distance - dx + 1;
+          final x = sensor.pos.x + dir.x * dx;
+          final y = sensor.pos.y + dir.y * dy;
+          if (minGrid <= x && x <= maxGrid && minGrid <= y && y <= maxGrid) {
+            final p = Vec2(x, y);
+            if (!sensors.any((s) => s.pos.manhattanDistanceTo(p) <= s.distance)) {
+              return p.xInt * 4000000 + p.yInt;
+            }
+          }
+        }
+      }
+    }
+  }
+
+  List<Sensor> parseSensors(String input) {
+    final sensorPattern = RegExp(r'Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)');
+    final List<Sensor> sensors = [];
+    for (final line in input.lines) {
+      final match = sensorPattern.firstMatch(line)!;
+      final Vec2 pos = Vec2.int(int.parse(match.group(1)!), int.parse(match.group(2)!));
+      final Vec2 beacon = Vec2.int(int.parse(match.group(3)!), int.parse(match.group(4)!));
+      sensors.add(Sensor(pos, beacon));
+    }
+    return sensors;
+  }
 }
 
+class Sensor {
+  Sensor(this.pos, this.beacon) {
+    distance = pos.manhattanDistanceTo(beacon).toInt();
+  }
+
+  final Vec2 pos;
+  final Vec2 beacon;
+  late final int distance;
+}
+
+
 // https://adventofcode.com/2022/day/15
-// 
+//
 // import 'dart:math';
-// 
+//
 // import 'package:aoc/aoc.dart';
 // import 'package:aoc/util/collection.dart';
 // import 'package:aoc/util/vec2.dart';
-// 
+//
 // class Day15 extends AdventDay {
 //   Day15() : super(2022, 15, solution1: 5125700, solution2: 11379394658764);
-// 
+//
 //   @override
 //   dynamic part1() {
 //     final sensors = inputSensors();
-// 
+//
 //     int minX = 0;
 //     int maxX = 0;
 //     for (final s in sensors) {
 //       minX = min(minX, s.pos.xInt - s.distance);
 //       maxX = max(maxX, s.pos.xInt + s.distance);
 //     }
-// 
+//
 //     bool noBeacon(Vec2 p) =>
 //       sensors.any((s) =>
 //         p != s.beacon &&
 //         s.pos.manhattanDistanceTo(p) <= s.distance
 //       );
-// 
+//
 //     final row = 2000000;
 //     return range(minX, maxX + 1)
 //       .map((col) => Vec2.int(col, row))
 //       .where(noBeacon)
 //       .length;
 //   }
-// 
+//
 //   @override
 //   dynamic part2() {
 //     final sensors = inputSensors();
 //     final minGrid = 0;
 //     final maxGrid = 4000000;
 //     final diamondDirs = [Vec2(-1, -1), Vec2(-1, 1), Vec2(1, -1), Vec2(1, 1)];
-// 
+//
 //     // Given that there is only one beacon that isn't seen by all sensors
 //     // in the grid, it must be at most sensor.distance+1 from all sensors.
 //     // If there was a beacon at more then that, then there would be more than
@@ -80,7 +155,7 @@ class Day15 extends AdventDay {
 //       }
 //     }
 //   }
-// 
+//
 //   List<Sensor> inputSensors() {
 //     final sensorPattern = RegExp(r'Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)');
 //     final List<Sensor> sensors = [];
@@ -93,14 +168,14 @@ class Day15 extends AdventDay {
 //     return sensors;
 //   }
 // }
-// 
+//
 // class Sensor {
 //   Sensor(this.pos, this.beacon) {
 //     distance = pos.manhattanDistanceTo(beacon).toInt();
 //   }
-// 
+//
 //   final Vec2 pos;
 //   final Vec2 beacon;
 //   late final int distance;
 // }
-// 
+//
--- packages/aoc2022/test/day15_test.dart
+++ packages/aoc2022/test/day15_test.dart
@@ -3,18 +3,34 @@ import 'package:test/test.dart';
 
 main() {
   group('2022 Day 15', () {
+    final exampleInput = '''
+Sensor at x=2, y=18: closest beacon is at x=-2, y=15
+Sensor at x=9, y=16: closest beacon is at x=10, y=16
+Sensor at x=13, y=2: closest beacon is at x=15, y=3
+Sensor at x=12, y=14: closest beacon is at x=10, y=16
+Sensor at x=10, y=20: closest beacon is at x=10, y=16
+Sensor at x=14, y=17: closest beacon is at x=10, y=16
+Sensor at x=8, y=7: closest beacon is at x=2, y=10
+Sensor at x=2, y=0: closest beacon is at x=2, y=10
+Sensor at x=0, y=11: closest beacon is at x=2, y=10
+Sensor at x=20, y=14: closest beacon is at x=25, y=17
+Sensor at x=17, y=20: closest beacon is at x=21, y=22
+Sensor at x=16, y=7: closest beacon is at x=15, y=3
+Sensor at x=14, y=3: closest beacon is at x=15, y=3
+Sensor at x=20, y=1: closest beacon is at x=15, y=3''';
+
     group('part 1', () {
-      // test('examples', () {
-      //   expect(Day15().part1(''), 0); 
-      // });
+      test('example', () {
+        expect(Day15().part1(exampleInput, 10), 26);
+      });
 
       test('solution', () => Day15().testPart1());
     });
 
     group('part 2', () {
-      // test('examples', () {
-      //   expect(Day15().part2(''), 0); 
-      // });
+      test('example', () {
+        expect(Day15().part2(exampleInput, 20), 56000011);
+      });
 
       test('solution', () => Day15().testPart2());
     });
