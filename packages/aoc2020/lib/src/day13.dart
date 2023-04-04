7fd12cf79fc5f860dcc8c5e8eee02a6c27b08443
Darren Austin <darren@darrenaustin.org>
2023-04-04 16:34:19 -0700

    Migrated 2020 13 from old repo.

---
+++ packages/aoc2020/lib/src/day13.dart
@@ -0,0 +1,73 @@
+// https://adventofcode.com/2020/day/13
+
+import 'package:aoc/aoc.dart';
+import 'package:aoc/util/string.dart';
+
+main() => Day13().solve();
+
+class Day13 extends AdventDay {
+  Day13() : super(
+    2020, 13, name: 'Shuttle Search',
+    solution1: 8063, solution2: 775230782877242,
+  );
+
+  @override
+  dynamic part1(String input) {
+    final timestamp = parseTimestamp(input);
+    final buses = parseBusIds(input);
+    int closestBus = buses.first;
+    int closestWaitTime = waitTime(buses.first, timestamp);
+    for (final bus in buses.skip(1)) {
+      final busWaitTime = waitTime(bus, timestamp);
+      if (busWaitTime < closestWaitTime) {
+        closestBus = bus;
+        closestWaitTime = busWaitTime;
+      }
+    }
+    return closestBus * closestWaitTime;
+  }
+
+  @override
+  dynamic part2(String input) {
+    final busOffsets = parseBusOffsets(input);
+    var timestamp = 0;
+    var increment = 1;
+    for (final busOffset in busOffsets) {
+      while ((timestamp + busOffset.offset) % busOffset.id != 0) {
+        timestamp += increment;
+      }
+      increment *= busOffset.id;
+    }
+    return timestamp;
+  }
+
+  int parseTimestamp(String input) => int.parse(input.lines[0]);
+
+  Iterable<int> parseBusIds(String input) =>
+    input.lines[1]
+      .split(',')
+      .where((e) => e != 'x')
+      .map(int.parse);
+
+  Iterable<int?> parseAllBusIds(String input) =>
+    input.lines[1]
+      .split(',')
+      .map((e) => e == 'x' ? null : int.parse(e));
+
+  Iterable<BusOffset> parseBusOffsets(String input) =>
+    parseAllBusIds(input).toList().asMap().entries
+      // Remove any entries that had an 'x' (which were translated into null ids)
+      .where((kv) => kv.value != null)
+      // The index for each entry is the offset for the timestamp (or the remainder)
+      .map((kv) => BusOffset(kv.value!, kv.key));
+
+  int waitTime(int bus, int timestamp) =>
+    ((timestamp ~/ bus) + 1) * bus - timestamp;
+}
+
+class BusOffset {
+  BusOffset(this.id, this.offset);
+
+  final int id;
+  final int offset;
+}
---
+++ packages/aoc2020/test/day13_test.dart
@@ -0,0 +1,31 @@
+import 'package:aoc2020/src/day13.dart';
+import 'package:test/test.dart';
+
+main() {
+  group('2020 Day 13', () {
+    final exampleInput = '''
+939
+7,13,x,x,59,x,31,19''';
+
+    group('part 1', () {
+      test('example', () {
+        expect(Day13().part1(exampleInput), 295);
+      });
+
+      test('solution', () => Day13().testPart1());
+    });
+
+    group('part 2', () {
+      test('example', () {
+        expect(Day13().part2(exampleInput), 1068781);
+        expect(Day13().part2('\n17,x,13,19'''), 3417);
+        expect(Day13().part2('\n67,7,59,61'), 754018);
+        expect(Day13().part2('\n67,x,7,59,61'), 779210);
+        expect(Day13().part2('\n67,7,x,59,61'), 1261476);
+        expect(Day13().part2('\n1789,37,47,1889'), 1202161486);
+      });
+
+      test('solution', () => Day13().testPart2());
+    });
+  });
+}
