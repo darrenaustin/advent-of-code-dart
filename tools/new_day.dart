3b2e38caf7607f83b9155553b9107d3869779235
Darren Austin <darren@darrenaustin.org>
2023-04-12 00:44:15 -0700

    Updated the new day script to work with the input repo.

--- tools/new_day.dart
+++ tools/new_day.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'dart:io';
+import 'package:aoc/aoc.dart';
 import 'package:args/args.dart';
 import 'package:path/path.dart' as path;
 
@@ -37,15 +38,15 @@ void main(List<String> arguments) async {
     out.writeln("main() => Day$day().solve();");
     out.writeln();
     out.writeln("class Day$day extends AdventDay {");
-    out.writeln("  Day$day() : super(");
-    out.writeln("    $year, $dayNum, name: '',");
-    out.writeln("  );");
+    out.writeln("  Day$day() : super($year, $dayNum, name: '');");
     out.writeln();
     out.writeln("  @override");
-    out.writeln("  dynamic part1(String input) => null;");
+    out.writeln("  dynamic part1(String input) {");
+    out.writeln("  }");
     out.writeln();
     out.writeln("  @override");
-    out.writeln("  dynamic part2(String input) => null;");
+    out.writeln("  dynamic part2(String input) {");
+    out.writeln("  }");
     out.writeln("}");
     await out.close();
   }
@@ -53,38 +54,52 @@ void main(List<String> arguments) async {
   // Update the day structures across the repo.
   await updateDayStructures();
 
-  // Create an empty input file
-  final inputFilePath = path.join(packagesPath, 'aoc$year', 'input', 'day$day.txt');
-  final inputFile = File(inputFilePath);
-  inputFile.createSync(recursive: true);
+  final sessionFile = File(path.join(Directory.current.path, ".session"));
 
-  // Try to copy the input file from the website.
-  String? sessionId;
-  File sessionFile = File("./.session");
-  if (sessionFile.existsSync()) {
-    sessionId = sessionFile.readAsStringSync().trim();
-  }
-  if (sessionId == null) {
-    print('No session data found, unable to download input file');
-  } else {
-    final inputURL = Uri.parse("https://adventofcode.com/$year/day/$dayNum/input");
-    print('Fetching input data: $inputURL');
-    final client = HttpClient();
-    try {
-      final request = await client.getUrl(inputURL);
-      request.cookies.add(Cookie("session", sessionId));
-      final response = await request.close();
-      if (response.statusCode == HttpStatus.ok) {
-        // Write the text to the input file
-        final contents = await response.transform(utf8.decoder).join();
-        inputFile.writeAsStringSync(contents);
-        print('Input data successfully downloaded');
-      } else {
-        print('Unable to fetch input data, code = ${response.statusCode}');
+  // Move into the package directory so that the relative
+  // input paths work correctly.
+  Directory.current = path.join(packagesPath, 'aoc$year');
+
+  // Create an empty input file
+  final inputFile = File(path.join(AdventDay.inputRepoBase, year, '${day}_input.txt'));
+  if (!inputFile.existsSync()) {
+    inputFile.createSync(recursive: true);
+
+    // Try to copy the input file from the website.
+    String? sessionId;
+    if (sessionFile.existsSync()) {
+      sessionId = sessionFile.readAsStringSync().trim();
+    }
+    if (sessionId == null) {
+      print('No session data found, unable to download input file');
+    } else {
+      final inputURL = Uri.parse("https://adventofcode.com/$year/day/$dayNum/input");
+      print('Fetching input data: $inputURL');
+      final client = HttpClient();
+      try {
+        final request = await client.getUrl(inputURL);
+        request.cookies.add(Cookie("session", sessionId));
+        final response = await request.close();
+        if (response.statusCode == HttpStatus.ok) {
+          // Write the text to the input file
+          final contents = await response.transform(utf8.decoder).join();
+          inputFile.writeAsStringSync(contents);
+          print('Input data successfully downloaded');
+        } else {
+          print('Unable to fetch input data, code = ${response.statusCode}');
+        }
+      } finally {
+        client.close();
       }
-    } finally {
-      client.close();
     }
   }
+
+  // Create an empty answer file
+  final answerFile = File(path.join(AdventDay.inputRepoBase, year, '${day}_answer.json'));
+  if (!answerFile.existsSync()) {
+    answerFile.createSync(recursive: true);
+    answerFile.writeAsStringSync('{\n}\n\n');
+  }
+
   exit(0);
 }
