f1a0c5c3e39f08878bc905eed9aab453eca126d1
Darren Austin <darren@darrenaustin.org>
2023-12-04 00:59:46 -0800

    Solved 2023 4

--- packages/aoc2023/lib/src/day04.dart
+++ packages/aoc2023/lib/src/day04.dart
@@ -9,64 +9,51 @@ import 'package:collection/collection.dart';
 main() => Day04().solve();
 
 class Day04 extends AdventDay {
-  Day04() : super(2023, 4, name: '');
+  Day04() : super(2023, 4, name: 'Scratchcards');
 
   @override
-  dynamic part1(String input) {
-    var sum = 0;
-    for (final cardData in input.lines) {
-      final match = RegExp(r'Card \s*[\d]+: (.*)').firstMatch(cardData);
-      final numberString = match!.group(1)!;
-      final parts = numberString.split(' | ');
-      final winningNumString = parts[0];
-      final winningNumbers =
-          winningNumString.trim().split(RegExp(r'\s+')).map(int.parse).toSet();
-      final pickedNumbersString = parts[1];
-      final pickedNumbers = pickedNumbersString
-          .trim()
-          .split(RegExp(r'\s+'))
-          .map(int.parse)
-          .toSet();
-      final correctNumbers = pickedNumbers.intersection(winningNumbers);
-      if (correctNumbers.isNotEmpty) {
-        sum += pow(2, correctNumbers.length - 1) as int;
-      }
-    }
-    return sum;
-  }
+  dynamic part1(String input) =>
+      input.lines.map(Card.parse).map((c) => c.score()).sum;
 
   @override
   dynamic part2(String input) {
-    final cardScores = <int>[];
-    final cards = <int>[];
-    for (final cardData in input.lines) {
-      final match = RegExp(r'Card \s*([\d]+): (.*)').firstMatch(cardData)!;
-      final numberString = match.group(2)!;
-      final parts = numberString.split(' | ');
-      final winningNumString = parts[0];
-      final winningNumbers =
-          winningNumString.trim().split(RegExp(r'\s+')).map(int.parse).toSet();
-      final pickedNumbersString = parts[1];
-      final pickedNumbers = pickedNumbersString
-          .trim()
-          .split(RegExp(r'\s+'))
-          .map(int.parse)
-          .toSet();
-      final correctNumbers = pickedNumbers.intersection(winningNumbers);
-      cards.add(1);
-      cardScores.add(correctNumbers.length);
-    }
+    final cards = input.lines.map(Card.parse).toList();
+    final numCards = List<int>.generate(cards.length, (i) => 1);
 
-    for (int i = 0; i < cardScores.length; i++) {
-      final cardsWon = cardScores[i];
-      if (cardsWon > 0) {
-        for (int copy = 0; copy < cards[i]; copy++) {
-          for (int j = 0; j < cardsWon; j++) {
-            cards[i + 1 + j]++;
-          }
-        }
+    for (int c = 0; c < cards.length; c++) {
+      final cardsWon = cards[c].numCorrect();
+      final copies = numCards[c];
+      for (int n = 0; n < cardsWon; n++) {
+        numCards[c + 1 + n] += copies;
       }
     }
-    return cards.sum;
+    return numCards.sum;
+  }
+}
+
+class Card {
+  Card({required this.winningNumbers, required this.pickedNumbers});
+
+  final Set<int> winningNumbers;
+  final Set<int> pickedNumbers;
+
+  static final spacesSeperator = RegExp(r'\s+');
+
+  factory Card.parse(String input) {
+    final match = RegExp(r'Card \s*([\d]+): (.*)').firstMatch(input)!;
+    final numberGroups = match.group(2)!.split(' | ');
+    final winningNumbers =
+        numberGroups[0].trim().split(spacesSeperator).map(int.parse).toSet();
+    final pickedNumbers =
+        numberGroups[1].trim().split(spacesSeperator).map(int.parse).toSet();
+
+    return Card(winningNumbers: winningNumbers, pickedNumbers: pickedNumbers);
+  }
+
+  int numCorrect() => pickedNumbers.intersection(winningNumbers).length;
+
+  int score() {
+    final correct = numCorrect();
+    return correct == 0 ? 0 : pow(2, correct - 1) as int;
   }
 }
